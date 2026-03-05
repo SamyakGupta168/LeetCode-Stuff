567da20a29ea72f9632342888301b443b0728c00
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-03-05 19:42:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (91.10%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -19,12 +19,28 @@ public:
         return true;
     }
 
+    bool dfs(auto src, vector<vector<int>>&graph, vector<int>&color) {
+        int node = src.first;
+        int parentColor = src.second;
+        if(parentColor==-1) color[node] = 0;
+        else color[node] = 1 - parentColor;
+        for(auto it : graph[node]) {
+            if(color[it]==-1) {
+                if(!dfs(make_pair(it, color[node]), graph, color)) return false;
+            } else if(color[it]==color[node]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> color(n, -1);
         for(int i=0;i<n;i++) {
             if(color[i]==-1) {
-                if(!bfs(i, graph, color)) return false;
+                // if(!bfs(i, graph, color)) return false;
+                if(!dfs(make_pair(i, -1), graph, color)) return false;
             }
         }
         return true;
