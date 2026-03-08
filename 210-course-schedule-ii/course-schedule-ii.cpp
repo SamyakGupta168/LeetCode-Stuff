30bb59e8b53613bd0575566bc795cd3bb19541e2
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-03-09 00:18:14 +0530

    Time: 3 ms (72.46%) | Memory: 18.3 MB (58.24%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void topo(int V, vector<vector<int>>&adj, vector<int>&indegree, vector<int>&ans) {
+        queue<int> q;
+        for(int i=0;i<V;i++) {
+            if(!indegree[i]) q.push(i);
+        }
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]) {
+                indegree[it]--;
+                if(!indegree[it]) q.push(it);
+            }
+        }
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int V = numCourses;
+        vector<vector<int>> adj(V);
+        for(auto &v : prerequisites) {
+            adj[v[1]].push_back(v[0]);
+        }
+        vector<int> indegree(V);
+        for(auto &v : adj) {
+            for(auto x : v) {
+                indegree[x]++;
+            }
+        }
+        vector<int> ans;
+        topo(V, adj, indegree, ans);
+        if(ans.size()<V) return {};
+        return ans;
+    }
+};
