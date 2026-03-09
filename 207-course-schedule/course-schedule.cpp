a4042aba3c0618dee89824ee52eac44d4622cec5
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-03-09 12:32:47 +0530

    Time: 10 ms (26.27%) | Memory: 19.3 MB (67.82%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    bool dfs(int node, int V, vector<vector<int>>&adj, vector<int>&vis, vector<int>&pathVis) {
-        vis[node]=1;
-        pathVis[node]=1;
-        for(auto it : adj[node]) {
-            if(!vis[it]) {
-                if(dfs(it, V, adj, vis, pathVis)) return true;
-            } else if(pathVis[it]) {
-                return true;
+    void topo(int V, vector<vector<int>>&adj, vector<int>&indegree, vector<int>&ans) {
+        queue<int> q;
+        for(int i=0;i<V;i++) {
+            if(!indegree[i]) {
+                q.push(i);
             }
         }
-        pathVis[node]=0;
-        return false;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]) {
+                indegree[it]--;
+                if(!indegree[it]) q.push(it);
+            }
+        }
     }
 
-    bool canFinish(int courses, vector<vector<int>>& preReq) {
-        int V = courses;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int V = numCourses;
         vector<vector<int>> adj(V);
-        for(auto &v : preReq) {
+        for(auto &v : prerequisites) {
             adj[v[1]].push_back(v[0]);
         }
-        vector<int> vis(V, 0), pathVis(V, 0);
-        for(int i=0;i<V;i++) {
-            if(!vis[i]) {
-                if(dfs(i, V, adj, vis, pathVis)) return false;
+
+        vector<int> indegree(V, 0);
+        for(auto &v : adj) {
+            for(auto x : v) {
+                indegree[x]++;
             }
         }
-        return true;
+
+        vector<int> ans;
+        topo(V, adj, indegree, ans);
+        return (ans.size()==V);
     }
 };
