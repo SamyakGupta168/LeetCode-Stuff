49a78aca747e8b7790827cd558a0d691d7d2d38f
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-03-09 23:21:15 +0530

    Time: 328 ms (26.69%) | Memory: 21.2 MB (43.62%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int bfs(string &beginWord, string &endWord, vector<string>&wordList) {
+        set<string> st(wordList.begin(), wordList.end());
+        queue<pair<string,int>> q;
+        q.push({beginWord, 1});
+        st.erase(beginWord);
+        while(!q.empty()) {
+            string node = q.front().first;
+            int level = q.front().second;
+            q.pop();
+            if(endWord==node) return level;
+            for(int i=0;i<node.size();i++) {
+                char orignal = node[i];
+                for(char ch='a';ch<='z';ch++) {
+                    node[i]=ch;
+                    if(st.find(node)!=st.end()) {
+                        q.push({node, level+1});
+                        st.erase(node);
+                    }
+                }
+                node[i]=orignal;
+            }
+        }
+        return 0;
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        return bfs(beginWord, endWord, wordList);
+    }
+};
