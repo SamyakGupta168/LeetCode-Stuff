bdc111c7ec50f6c2407d7cc487c5548a0faa6a70
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-03-18 19:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (55.63%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    using ll = long long;
+    ll Msb(ll n) {
+        if(n==0) return 0;
+        ll pos=0;
+        while(n>0) {
+            pos++;
+            n/=10;
+        }
+        return pos;
+    }
+
+    ll expo(int x) {
+        ll val = 1;
+        while(x--) {
+            val*=10;
+        }
+        return val;
+    }
+
+    long long countCommas(long long n) {
+        string s = to_string(n);
+        ll msb = s.size()-1;
+        ll ans = (n - expo(msb) + 1)*(msb/3);
+        msb--;
+        while(msb>=3) {
+            ans+=(9*expo(msb)*(msb/3));
+            msb--;
+        }
+        return ans;
+    }
+};
