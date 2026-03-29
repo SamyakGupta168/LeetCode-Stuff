9da3fb20cc2319f84d875967c13a04f917642ded
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-03-29 10:04:16 +0530

    Time: 560 ms (5.05%) | Memory: 356.4 MB (5.30%) - LeetSync

---
+++ 4267-design-event-manager/design-event-manager.cpp
@@ -0,0 +1,43 @@
+class EventManager {
+public:
+    set<int> activeEvents;
+    map<int,set<int>> list;
+    map<int,int> priority;
+    
+    EventManager(vector<vector<int>>& events) {
+        for(auto &v : events) {
+            activeEvents.insert(v[0]);
+            list[v[1]].insert(v[0]);
+            priority[v[0]] = v[1];
+        }
+    }
+    
+    void updatePriority(int eventId, int newPriority) {
+        if(activeEvents.count(eventId)) {
+            int oldPriority = priority[eventId];
+            list[oldPriority].erase(eventId);
+            if(list[oldPriority].size()==0) list.erase(oldPriority);
+            priority[eventId] = newPriority;
+            list[newPriority].insert(eventId);
+        }
+    }
+    
+    int pollHighest() {
+        if(activeEvents.size()==0) return -1;
+        auto it = list.rbegin();
+        int highestPriority = it->first;
+        int lowestId = *list[highestPriority].begin();
+        list[highestPriority].erase(lowestId);
+        if(list[highestPriority].size()==0) list.erase(highestPriority);
+        priority.erase(lowestId);
+        activeEvents.erase(lowestId);
+        return lowestId;
+    }
+};
+
+/**
+ * Your EventManager object will be instantiated and called as such:
+ * EventManager* obj = new EventManager(events);
+ * obj->updatePriority(eventId,newPriority);
+ * int param_2 = obj->pollHighest();
+ */
