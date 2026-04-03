0a9889f04078504a4a381a29040ef37209994323
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-04-03 17:11:28 +0530

    Time: 28 ms (29.48%) | Memory: 23.6 MB (74.82%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-    int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
-    int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
-    
-    void dijkstra(auto src, vector<vector<int>>&grid, vector<vector<int>>&dist) {
-        int n=grid.size(), m=grid[0].size();
-        dist[src.first][src.second] = 1;
-        // priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        queue<pair<int,pair<int,int>>> pq;
-        pq.push({1, src});
-        
+    int dx[8] = {0, 0, 1, 1, 1, -1, -1, -1};
+    int dy[8] = {1, -1, 0, 1, -1, 0, 1, -1};
+
+    int dijkstra(vector<vector<int>>&grid) {
+        int n = grid.size();
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        dist[0][0] = 0;
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
+        pq.push({0, {0, 0}});
         while(!pq.empty()) {
-            int dis = pq.front().first;
-            auto [x, y] = pq.front().second;
+            int dis = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
             pq.pop();
-            
+
             for(int i=0;i<8;i++) {
-                int nx = x + dx[i], ny = y + dy[i];
-                if(nx>=0 && ny>=0 && nx<n && ny<m) {
-                    if(grid[nx][ny] == 1) {
-                        if(dis + 1 < dist[nx][ny]) {
-                            dist[nx][ny] = dis + 1;
-                            pq.push({dist[nx][ny], {nx, ny}});
-                        }
+                int nr = r + dx[i], nc = c + dy[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<n) {
+                    if(grid[nr][nc] == 1) continue;
+                    if(dis + 1 < dist[nr][nc]) {
+                        dist[nr][nc] = dis + 1;
+                        pq.push({dist[nr][nc], {nr, nc}});
                     }
                 }
             }
         }
+        
+        return (dist[n-1][n-1] == INT_MAX) ? -1 : dist[n-1][n-1] + 1;
     }
 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size(), m=grid[0].size();
-        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
-        auto src = make_pair(0, 0);
-        auto desti = make_pair(n-1, m-1);
 
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) grid[i][j] = 1 - grid[i][j];
-        }
-
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        
-        dijkstra(src, grid, dist);
-        if(dist[desti.first][desti.second] == INT_MAX) return -1;
-        return dist[desti.first][desti.second];
+        return dijkstra(grid);
     }
 };
