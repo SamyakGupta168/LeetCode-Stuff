ce0b11f3223879e23dfbce76026327e5663aec67
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-04-04 19:44:32 +0530

    Time: 295 ms (5.18%) | Memory: 113.3 MB (5.55%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         int n = s1.size();
-        multiset<char> ms1, ms2;
+        unordered_multiset<char> ms1, ms2;
         bool flag = true;
         for(int i=0;i<n;i++) {
             if(i%2==0) {
