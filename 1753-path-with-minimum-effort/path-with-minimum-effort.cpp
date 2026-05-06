aa0294073a57fd8e0665ef830c48da24f346b1ae
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-06 23:01:59 +0530

    Time: 43 ms (86.18%) | Memory: 24.5 MB (57.07%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -5,25 +5,25 @@ public:
 
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size(), m = heights[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        vector<vector<int>> diff(n, vector<int>(m, INT_MAX));
         priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
         pq.push({0, {0, 0}});
-
+        diff[0][0] = 0;
         while(!pq.empty()) {
-            int diff = pq.top().first;
+            int d = pq.top().first;
             int r = pq.top().second.first;
             int c = pq.top().second.second;
             pq.pop();
-            if(r==n-1 && c==m-1) return diff;
-            
+            if(r==n-1 && c==m-1) return d;
             for(int i=0;i<4;i++) {
                 int nr = r + dx[i], nc = c + dy[i];
                 if(nr>=0 && nc>=0 && nr<n && nc<m) {
-                    int d = abs(heights[r][c] - heights[nr][nc]);
-                    if(max(diff, d) < dist[nr][nc]) {
-                        dist[nr][nc] = max(diff, d);
-                        pq.push({dist[nr][nc], {nr, nc}});
+                    int nd = abs(heights[r][c]-heights[nr][nc]);
+                    if(max(nd, d) < diff[nr][nc]) {
+                        diff[nr][nc] = max(nd, d);
+                        pq.push({diff[nr][nc], {nr, nc}});
                     }
+                    
                 }
             }
         }
