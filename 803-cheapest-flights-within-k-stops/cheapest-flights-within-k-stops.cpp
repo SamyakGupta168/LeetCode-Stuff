da42823c92724c42df63d70acac27f1fe6b9dd67
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-06 23:42:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (72.29%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> adj(n);
+        vector<vector<pair<int, int>>> adj(n);
         for(auto &v : flights) {
             adj[v[0]].push_back({v[1], v[2]});
         }
 
         vector<int> dist(n, INT_MAX);
-        queue<pair<pair<int,int>, int>> q;
+        priority_queue<pair<pair<int,int>, int>, vector<pair<pair<int,int>, int>>, greater<pair<pair<int,int>, int>>> pq;
+        // Priority queue will store this {{stops, dist}, node}
         dist[src] = 0;
-        q.push({{0, src}, 0});
+        pq.push({{0, 0}, src});
+        int ans = INT_MAX;
+        while(!pq.empty()) {
+            int stops = pq.top().first.first;
+            int dis = pq.top().first.second;
+            int node = pq.top().second;
+            pq.pop();
 
-        while(!q.empty()) {
-            int stops = q.front().first.first;
-            int node = q.front().first.second;
-            int dis = q.front().second;
-            q.pop();
-            if(stops == k+1) continue;
+            if(node == dst) {
+                if(stops > k) continue;
+                else ans = min(ans, dis);
+            }
 
             for(auto &it : adj[node]) {
                 int adjNode = it.first;
                 int edgeWeight = it.second;
                 if(dis + edgeWeight < dist[adjNode]) {
                     dist[adjNode] = dis + edgeWeight;
-                    q.push({{stops+1, adjNode}, dist[adjNode]});
+                    if(adjNode == dst) pq.push({{stops, dist[adjNode]}, adjNode});
+                    else pq.push({{stops+1, dist[adjNode]}, adjNode});
                 }
             }
         }
 
-        return (dist[dst] == INT_MAX) ? -1 : dist[dst];
+        return ((ans == INT_MAX) ? -1 : ans);
     }
 };
