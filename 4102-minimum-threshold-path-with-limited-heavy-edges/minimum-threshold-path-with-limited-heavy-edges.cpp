388cd05fcc97e4b0d27893a02971f51ef7d795f2
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-19 00:54:17 +0530

    Time: 233 ms (42.76%) | Memory: 114.5 MB (75.31%) - LeetSync

--- 4102-minimum-threshold-path-with-limited-heavy-edges/minimum-threshold-path-with-limited-heavy-edges.cpp
+++ 4102-minimum-threshold-path-with-limited-heavy-edges/minimum-threshold-path-with-limited-heavy-edges.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    bool dijkstra(int V, vector<vector<pair<int,int>>> &adj, int src, int tar, int th, int k) {
+    bool dijkstra(int V, vector<vector<pair<int, int>>>& adj, int src, int tar,
+                  int th, int k) {
 
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    vector<int> dist(V, INT_MAX);
-    dist[src] = 0;
-    pq.push({0, src});
-    while(!pq.empty()) {
-        int dis = pq.top().first;
-        int node = pq.top().second;
-        pq.pop();
-        
-        for(auto &it : adj[node]) {
-            int edw = (it.second > th);
-            int adjNode = it.first;
-            if(dis + edw < dist[adjNode]) {
-                dist[adjNode] = dis + edw;
-                pq.push({dist[adjNode], adjNode});
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        vector<int> dist(V, INT_MAX);
+        dist[src] = 0;
+        pq.push({0, src});
+        while (!pq.empty()) {
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for (auto& it : adj[node]) {
+                int edw = (it.second > th);
+                int adjNode = it.first;
+                if (dis + edw < dist[adjNode]) {
+                    dist[adjNode] = dis + edw;
+                    pq.push({dist[adjNode], adjNode});
+                }
             }
         }
-    }
 
         return (dist[tar] <= k);
     }
 
-    int minimumThreshold(int n, vector<vector<int>>& edges, int src, int tar, int k) {
-        vector<vector<pair<int,int>>> adj(n);
-        for(auto &v : edges) {
+    int minimumThreshold(int n, vector<vector<int>>& edges, int src, int tar,
+                         int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& v : edges) {
             adj[v[0]].push_back({v[1], v[2]});
             adj[v[1]].push_back({v[0], v[2]});
         }
 
         int low = 0, high = 1e9 + 1;
         int ans = -1;
-        while(low <= high) {
-            int mid = low + (high-low)/2;
-            if(dijkstra(n, adj, src, tar, mid, k)) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (dijkstra(n, adj, src, tar, mid, k)) {
                 ans = mid;
                 high = mid - 1;
             } else {
