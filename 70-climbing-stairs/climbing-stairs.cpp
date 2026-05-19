1df7fff648d753f4fb7676c6836ac7065ece693a
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-19 12:25:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.68%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int f(int idx, vector<int>&dp) {
+        if(idx <= 1) return 1;
+        if(dp[idx] != -1) return dp[idx];
+        return dp[idx] = f(idx-1, dp) + f(idx-2, dp);
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return f(n, dp);
+    }
+};
