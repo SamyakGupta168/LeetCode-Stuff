622d90587bde62820df7889d428b8405eda96545
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-19 09:40:32 +0530

    Time: 1279 ms (5.02%) | Memory: 126.6 MB (5.18%) - LeetSync

---
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    using ll = long long;
+
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+        // Once an index is visited we can't go back, we can only go forward
+        vector<ll> jump(n, -1);
+        priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
+        pq.push({0, 0}); // {steps, idx}
+        jump[0] = 0;
+        while(!pq.empty()) {
+            int steps = pq.top().first;
+            int idx = pq.top().second;
+            pq.pop();
+
+            for(int i=idx+1;i<n;i++) {
+                ll diff = (ll)nums[i]-nums[idx];
+                if(diff<=target && diff>=(-target)) {
+                    if(jump[i] < jump[idx] + 1) {
+                        jump[i] = jump[idx]+1;
+                        pq.push({jump[i], i});
+                    }
+                }
+            }
+        }
+
+        return (jump[n-1] != -1) ? jump[n-1] : -1;
+    }
+};
