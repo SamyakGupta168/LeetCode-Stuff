e7a9a4248a12359c3f2ad6e6ee96f9dbbf81f527
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-19 09:41:18 +0530

    Time: 1554 ms (5.02%) | Memory: 70.5 MB (5.89%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -6,7 +6,7 @@ public:
         int n = nums.size();
         // Once an index is visited we can't go back, we can only go forward
         vector<ll> jump(n, -1);
-        priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
+        priority_queue<pair<ll,ll>> pq;
         pq.push({0, 0}); // {steps, idx}
         jump[0] = 0;
         while(!pq.empty()) {
