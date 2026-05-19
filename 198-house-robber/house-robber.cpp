62e62420782b6df10f175927e000e41284e93c4c
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-19 17:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (20.52%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int f(int idx, vector<int>&nums, vector<int>&dp) {
+        if(idx < 0) return 0;
+        if(idx == 0) return nums[0];
+        if(dp[idx] != -1) return dp[idx];
+
+        int sum1 = nums[idx] + f(idx-2, nums, dp);
+        int sum2 = f(idx-1, nums, dp);
+        return dp[idx] = max(sum1, sum2);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return f(n-1, nums, dp);
+    }
+};
