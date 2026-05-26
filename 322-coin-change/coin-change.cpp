7e05ba67283ddfd4af2a5f0041fec4caa3764442
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-26 19:03:13 +0530

    Time: 32 ms (48.96%) | Memory: 22.1 MB (42.92%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    int f(int idx, int T, vector<int>&coins, vector<vector<int>>&dp) {
-        if(idx == 0) {
-            if(T % coins[0] == 0) return T/coins[0];
-            return 1e9;
-        }
-        if(dp[idx][T] != -1) return dp[idx][T];
-        int notTake = f(idx-1, T, coins, dp);
-        int take = INT_MAX;
-        if(coins[idx] <= T) {
-            take = 1 + f(idx, T-coins[idx], coins, dp);
-        }
-        return dp[idx][T] = min(take, notTake);
-    }
 
     int coinChange(vector<int>& coins, int amt) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amt+1, 1e9));
+        vector<int> prev(amt+1, 1e9), curr(amt+1, 1e9);
         
         for(int i=0;i<=amt;i++) {
-            if(i % coins[0] == 0) dp[0][i] = i/coins[0];
+            if(i % coins[0] == 0) prev[i] = i/coins[0];
         }
 
         for(int i=1;i<n;i++) {
             for(int j=0;j<=amt;j++) {
-                int notTake = 0 + dp[i-1][j];
+                int notTake = 0 + prev[j];
                 int take = INT_MAX;
                 if(coins[i] <= j) {
-                    take = 1 + dp[i][j - coins[i]];
+                    take = 1 + curr[j - coins[i]];
                 }
-                dp[i][j] = min(take, notTake);
+                curr[j] = min(take, notTake);
             }
+            prev = curr;
         }
 
-        if(dp[n-1][amt] != 1e9) return dp[n-1][amt];
+        if(prev[amt] != 1e9) return prev[amt];
         return -1;
     }
 };
