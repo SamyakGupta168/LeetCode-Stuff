f1c809f5244b662e2277791a203131d02515d3b1
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-26 23:19:41 +0530

    Time: 22 ms (71.36%) | Memory: 10.8 MB (72.01%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int f(int idx, int T, vector<vector<int>>&dp, vector<int>&coins) {
-        if(T == 0) return 1;
-        if(idx == 0) {
-            return (T%coins[0] == 0);
-        }
-        if(dp[idx][T] != -1) return dp[idx][T];
-        int notTake = f(idx-1, T, dp, coins);
-        int take = 0;
-        if(coins[idx] <= T) {
-            take = f(idx, T - coins[idx], dp, coins);
-        }
-        return dp[idx][T] = take + notTake;
-    }
 
     int change(int amt, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amt+1, -1));
-        return f(n-1, amt, dp, coins);
+        vector<long long> prev(amt+1, 0), curr(amt+1, 0);
+        prev[0] = 1, curr[0] = 1;
+
+        for(int i=0;i<=amt;i++) {
+            if(i % coins[0] == 0) prev[i] = 1; 
+        }
+
+        for(int i=1;i<n;i++) {
+            for(int j=1;j<=amt;j++) {
+                int notTake = prev[j];
+                int take = 0;
+                if(coins[i] <= j) {
+                    take = curr[j - coins[i]];
+                }
+                curr[j] = (long long)take + notTake;
+            }
+            prev = curr;
+        }
+
+        return prev[amt];
     }
 };
