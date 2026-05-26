6afa82a09d961976806202369bf9d655688caedc
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-26 11:32:19 +0530

    Time: 38 ms (81.56%) | Memory: 20.9 MB (70.75%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int f(int i, int j1, int j2, vector<vector<int>>&grid, vector<vector<vector<int>>>&dp, int n, int m) {
-        if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return -1e8;
+        if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return 0;
         if(i == n-1) {
             if(j1 == j2) return grid[i][j1];
             else return grid[i][j1] + grid[i][j2];
