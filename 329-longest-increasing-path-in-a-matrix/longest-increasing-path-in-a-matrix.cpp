6630fc78ff6d4340327b52439d5de87109920b14
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-29 20:34:17 +0530

    Time: 15 ms (44.17%) | Memory: 23.5 MB (23.48%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,33 +1,45 @@
 class Solution {
 public:
-    int dx[4] = {0, 0, 1, -1};
+    int dx[4] = {0, 0, -1, 1};
     int dy[4] = {1, -1, 0, 0};
 
-    int f(int i, int j, vector<vector<int>>&mat, vector<vector<int>>&dp, int n, int m) {
-        if(dp[i][j] != -1) return dp[i][j];
+    int longestIncreasingPath(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> indegree(n, vector<int>(m, 0));
 
-        int maxi = 1;
-        for(int k=0;k<4;k++) {
-            int ni = i + dy[k], nj = j + dx[k];
-            if(ni>=0 && nj>=0 && ni<n && nj<m) {
-                if(mat[i][j] < mat[ni][nj]) {
-                    maxi = max(maxi, 1 + f(ni, nj, mat, dp, n, m));
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                for(int k=0;k<4;k++) {
+                    int ni = i + dy[k], nj = j + dx[k];
+                    if(ni>=0 && nj>=0 && nj<m && ni<n) {
+                        if(mat[i][j] < mat[ni][nj]) indegree[ni][nj]++;
+                    }
                 }
             }
         }
 
-        return dp[i][j] = maxi;
-    }
-
-    int longestIncreasingPath(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-
-        int ans = 0;
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(dp[i][j] == -1) {
-                    ans = max(ans, f(i, j, mat, dp, n, m));
+                if(!indegree[i][j]) q.push({{i, j}, 1});
+            }
+        }
+
+        int ans = 0;
+        while(!q.empty()) {
+            auto [r, c] = q.front().first;
+            int level = q.front().second;
+            q.pop();
+            ans = max(ans, level);
+
+            for(int k=0;k<4;k++) {
+                int nr = r + dy[k], nc = c + dx[k];
+                if(nr>=0 && nc>=0 && nr<n && nc<m) {
+                    if(mat[r][c] < mat[nr][nc]) {
+                        indegree[nr][nc]--;
+                        if(!indegree[nr][nc]) q.push({{nr, nc}, level+1});
+                    }
                 }
             }
         }
