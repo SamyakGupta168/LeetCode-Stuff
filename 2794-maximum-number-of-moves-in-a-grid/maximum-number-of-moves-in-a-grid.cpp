4b29a371fb3a0798fdd57719352a059e3cb0570a
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-29 22:48:55 +0530

    Time: 34 ms (23.08%) | Memory: 74.6 MB (65.57%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&dp, int n, int m) {
-        if(i < 0 || j < 0 || i >= n || j >= m) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int cnt = 1;
-        if(i-1>=0 && j+1<m && grid[i][j] < grid[i-1][j+1]) cnt = max(cnt, 1 + f(i-1, j+1, grid, dp, n, m));
-        if(j+1<m && grid[i][j] < grid[i][j+1]) cnt = max(cnt, 1 + f(i, j+1, grid, dp, n, m));
-        if(i+1<n && j+1<m && grid[i][j] < grid[i+1][j+1]) cnt = max(cnt, 1 + f(i+1, j+1, grid, dp, n, m));
-        return dp[i][j] = cnt;
-    }
 
     int maxMoves(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        int ans = 0;
         for(int i=0;i<n;i++) {
-            ans = max(ans, f(i, 0, grid, dp, n, m));
+            dp[i][m-1] = 1;
+        }
+
+        int ans = 0;
+        for(int j=m-2;j>=0;j--) {
+            for(int i=0;i<n;i++) {
+                int cnt = 1;
+                if(i-1>=0 && j+1<m && grid[i][j] < grid[i-1][j+1]) cnt = max(cnt, 1 + dp[i-1][j+1]);
+                if(j+1<m && grid[i][j] < grid[i][j+1]) cnt = max(cnt, 1 + dp[i][j+1]);
+                if(i+1<n && j+1<m && grid[i][j] < grid[i+1][j+1]) cnt = max(cnt, 1 + dp[i+1][j+1]);
+                dp[i][j] = cnt;
+                if(j == 0) ans = max(ans, dp[i][0]);
+            }
         }
 
         return ans - 1;
