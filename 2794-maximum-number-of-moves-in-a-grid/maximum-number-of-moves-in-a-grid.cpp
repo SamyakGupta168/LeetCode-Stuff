b73197b2c92db592073a60863636eb995019e8d6
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-29 22:51:11 +0530

    Time: 23 ms (34.43%) | Memory: 68.2 MB (90.11%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -3,21 +3,22 @@ public:
 
     int maxMoves(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<int> prev(n, -1), curr(n, -1);
         for(int i=0;i<n;i++) {
-            dp[i][m-1] = 1;
+            prev[i] = 1;
         }
 
         int ans = 0;
         for(int j=m-2;j>=0;j--) {
             for(int i=0;i<n;i++) {
                 int cnt = 1;
-                if(i-1>=0 && j+1<m && grid[i][j] < grid[i-1][j+1]) cnt = max(cnt, 1 + dp[i-1][j+1]);
-                if(j+1<m && grid[i][j] < grid[i][j+1]) cnt = max(cnt, 1 + dp[i][j+1]);
-                if(i+1<n && j+1<m && grid[i][j] < grid[i+1][j+1]) cnt = max(cnt, 1 + dp[i+1][j+1]);
-                dp[i][j] = cnt;
-                if(j == 0) ans = max(ans, dp[i][0]);
+                if(i-1>=0 && j+1<m && grid[i][j] < grid[i-1][j+1]) cnt = max(cnt, 1 + prev[i-1]);
+                if(j+1<m && grid[i][j] < grid[i][j+1]) cnt = max(cnt, 1 + prev[i]);
+                if(i+1<n && j+1<m && grid[i][j] < grid[i+1][j+1]) cnt = max(cnt, 1 + prev[i+1]);
+                curr[i] = cnt;
+                if(j == 0) ans = max(ans, curr[i]);
             }
+            prev = curr;
         }
 
         return ans - 1;
