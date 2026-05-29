b4e7d617f1a088a5e3f40cccee8bc1360862df02
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-29 20:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (60.31%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+
+    int f(int i, int j, vector<vector<int>>&mat, vector<vector<int>>&dp, int n, int m) {
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int maxi = 1;
+        for(int k=0;k<4;k++) {
+            int ni = i + dy[k], nj = j + dx[k];
+            if(ni>=0 && nj>=0 && ni<n && nj<m) {
+                if(mat[i][j] < mat[ni][nj]) {
+                    int org = mat[i][j];
+                    mat[i][j] = -1;
+                    maxi = max(maxi, 1 + f(ni, nj, mat, dp, n, m));
+                    mat[i][j] = org;
+                }
+            }
+        }
+
+        return dp[i][j] = maxi;
+    }
+
+    int longestIncreasingPath(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(dp[i][j] == -1) {
+                    ans = max(ans, f(i, j, mat, dp, n, m));
+                }
+            }
+        }
+
+        return ans;
+    }
+};
