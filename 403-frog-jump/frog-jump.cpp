a6399f7d39a4130ec1cabd6305ee79f5fc8a1936
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-05-30 00:00:30 +0530

    Time: 2095 ms (5.07%) | Memory: 261.5 MB (9.10%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int f(int i, int k, vector<int>&stones, vector<vector<int>>&dp, map<int,int>&idx) {
-        if(i == stones.size()-1) return 1;
-        if(dp[i][k] != -1) return dp[i][k];
-
-        int isPoss = 0;
-        if(i != 0 && idx.count(stones[i]+k-1) && idx[stones[i]+k-1] > i) isPoss |= f(idx[stones[i]+k-1], k-1, stones, dp, idx);
-        if(idx.count(stones[i]+k) && idx[stones[i]+k] > i) isPoss |= f(idx[stones[i]+k], k, stones, dp, idx);
-        if(i != 0 && idx.count(stones[i]+k+1) && idx[stones[i]+k+1] > i) isPoss |= f(idx[stones[i]+k+1], k+1, stones, dp, idx);
-        return dp[i][k] = isPoss;
-    }
 
     bool canCross(vector<int>& stones) {
         int n = stones.size();
-        map<int,int> idx;
+        unordered_map<int,int> idx;
         for(int i=0;i<stones.size();i++) {
             idx[stones[i]] = i;
         }
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i=1;i<n;i++) dp[n-1][i] = 1;
 
-        return f(0, 1, stones, dp, idx);
+        for(int i=n-2;i>=0;i--) {
+            for(int k=1;k<n;k++) {
+                if(i == 0 && k>1) break;
+                int isPoss = 0;
+                if(i != 0 && idx.count(stones[i]+k-1) && idx[stones[i]+k-1] > i) isPoss |= dp[idx[stones[i]+k-1]][k-1];
+                if(idx.count(stones[i]+k) && idx[stones[i]+k] > i) isPoss |= dp[idx[stones[i]+k]][k];
+                if(k < n-1 && i != 0 && idx.count(stones[i]+k+1) && idx[stones[i]+k+1] > i) isPoss |= dp[idx[stones[i]+k+1]][k+1];
+                dp[i][k] = isPoss;
+            }
+        }
+
+        return dp[0][1];
     }
 };
