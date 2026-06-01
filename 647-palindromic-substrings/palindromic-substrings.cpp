7326cdc979538e96523c1fce7d475963affb164a
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-01 17:21:41 +0530

    Time: 22 ms (38.15%) | Memory: 34.6 MB (17.99%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int f(int i, int j, string &s, vector<vector<int>>&dp) {
-        if(i+1 == j && s[i]==s[j]) return 1;
-        if(i == j) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = ((s[i]==s[j])&&f(i+1, j-1, s, dp));
-    }
 
     int countSubstrings(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for(int i=0;i<n;i++) dp[i][i] = 1;
+        for(int i=1;i<n;i++) {
+            if(s[i-1]==s[i]) dp[i-1][i] = 1;
+        }
+
         int cnt = 0;
-        for(int i=0;i<n;i++) {
+        for(int i=n-1;i>=0;i--) {
             for(int j=i;j<n;j++) {
-                if(dp[i][j] == -1) cnt += f(i, j, s, dp);
-                else cnt += dp[i][j];
+                if(i == j) {
+                    cnt += dp[i][j];
+                    continue;
+                }
+                else if(j-i == 1) {
+                    cnt += dp[i][j];
+                    continue;
+                }
+
+                dp[i][j] = (s[i]==s[j])&&dp[i+1][j-1];
+                cnt += dp[i][j];
             }
         }
 
