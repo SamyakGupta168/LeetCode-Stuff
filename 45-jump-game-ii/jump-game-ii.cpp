741d6ec63c5e9bca37111407c3f244e069dd4c7c
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-07 19:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (33.66%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int jump(vector<int>& arr) {
         int n = arr.size();
-        vector<int> dp(n, -1);
-        dp[n-1] = 0;
-        for(int i = n-2;i>=0;i--) {
-            int minJump = 1e9;
-            for(int jumpIdx=i+1;jumpIdx<=min(i+arr[i], n-1);jumpIdx++) {
-                minJump = min(minJump, 1 + dp[jumpIdx]);
+        if(n == 1) return 0;
+        if(arr[0] == 0) return -1;
+        int jump = 0, currReach = 0, maxReach = 0;
+        for(int i=0;i<n;i++) {
+            maxReach = max(maxReach, i + arr[i]);
+            if(maxReach >= n-1) {
+                return jump + 1;
             }
-            dp[i] = minJump;
+            if(i == currReach) {
+                if(maxReach == i) {
+                    return -1;
+                }
+                jump++;
+                currReach = maxReach;
+            }
         }
         
-        return (dp[0] != 1e9 ? dp[0] : -1);
+        return -1;
     }
 };
