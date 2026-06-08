79e1c965db685247e6c1bb2f97fdd4f45f87980d
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-08 23:29:08 +0530

    Time: 39 ms (76.72%) | Memory: 18.7 MB (72.39%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -11,38 +11,19 @@ public:
         return (ind == s1.size());
     }
 
-    int f(int idx, int next, vector<string>&words, vector<vector<int>>&dp) {
-        int n = words.size();
-        if(idx < 0) return 0;
-        if(dp[idx][next] != -1) return dp[idx][next];
-
-        int notTake = 0 + f(idx-1, next, words, dp);
-        int take = 0;
-        if(next == n || check(words[idx], words[next])) {
-            take = 1 + f(idx-1, idx, words, dp);
-        }
-        
-        return dp[idx][next] = max(take, notTake);
-    }
-
     int longestStrChain(vector<string>& words) {
         int n = words.size();
         sort(words.begin(), words.end(), [&] (auto &s1, auto &s2) -> bool {
             return (s1.size() < s2.size());
         });
 
-        vector<vector<int>> dp(n+1, vector<int>(n+2, 0));
-        for(int i=1;i<=n;i++) {
-            for(int next=i+1;next<=n+1;next++) {
-                int notTake = dp[i-1][next];
-                int take = 0;
-                if(next == n+1 || check(words[i-1], words[next-1])) {
-                    take = 1 + dp[i-1][i];
-                }
-                dp[i][next] = max(take, notTake);
+        vector<int> dp(n, 1);
+        for(int i=0;i<n;i++) {
+            for(int prev=0;prev<i;prev++) {
+                if(check(words[prev], words[i])) dp[i] = max(dp[i], 1 + dp[prev]);
             }
         }
 
-        return dp[n][n+1];
+        return *max_element(dp.begin(), dp.end());
     }
 };
