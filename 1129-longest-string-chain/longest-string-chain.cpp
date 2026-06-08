cf2617a0fc57e70b973df0ce6a343968988c3988
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-08 23:08:06 +0530

    Time: 186 ms (29.88%) | Memory: 105.9 MB (30.77%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
     bool check(string &s1, string &s2) {
-        if(s1.size() != (s2.size() + 1)) return false;
-        int i = 0, j = 0;
-        while(i<s2.size() && j<s1.size()) {
-            if(s2[i] == s1[j]) i++;
-            j++;
+        if(s1.size() + 1 != s2.size()) return false;
+        int ind = 0;
+        for(auto ch : s2) {
+            if(ind == s1.size()) break;
+            if(ch == s1[ind]) ind++;
         }
-        return (i == s2.size());
+
+        return (ind == s1.size());
     }
 
-    int f(int idx, vector<string>& words, vector<int>&dp, int V) {
-        if(idx == V-1) return 1;
-        if(dp[idx] != -1) return dp[idx];
+    int f(int idx, int next, vector<string>&words, vector<vector<int>>&dp) {
+        int n = words.size();
+        if(idx < 0) return 0;
+        if(dp[idx][next] != -1) return dp[idx][next];
 
-        int maxi = 1;
-        for(int i=idx+1;i<V;i++) {
-            if(check(words[idx], words[i])) {
-                maxi = max(maxi, 1 + f(i, words, dp, V));
-            }
+        int notTake = 0 + f(idx-1, next, words, dp);
+        int take = 0;
+        if(next == n || check(words[idx], words[next])) {
+            take = 1 + f(idx-1, idx, words, dp);
         }
-        return dp[idx] = maxi;
+        
+        return dp[idx][next] = max(take, notTake);
     }
 
     int longestStrChain(vector<string>& words) {
-        int V = words.size();
-        
+        int n = words.size();
         sort(words.begin(), words.end(), [&] (auto &s1, auto &s2) -> bool {
-            return (s1.size() > s2.size());
+            return (s1.size() < s2.size());
         });
 
-        int ans = 0;
-        vector<int> dp(V, -1);
-        for(int i=0;i<V;i++) {
-            if(dp[i] == -1) {
-                ans = max(ans, f(i, words, dp, V));
-            }
-        }
-
-        return ans;
-        
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return f(n-1, n, words, dp);
     }
 };
