31368a742fa0468a18410f194e4fb60743cb2a9a
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-10 23:33:57 +0530

    Time: 119 ms (51.98%) | Memory: 33.5 MB (14.09%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    bool isPalindrome(string &s) {
-        int l = 0, r = s.size() - 1;
-        while(l < r) {
-            if(s[l] != s[r]) return false;
-            l++, r--;
-        }
-        return true;
-    }
 
     int minCut(string s) {
         int n = s.size();
+        vector<vector<bool>> isPal(n, vector<bool>(n, 0));
+
+        for(int i=0;i<n;i++) isPal[i][i] = 1;
+        for(int i=n-1;i>=0;i--) {
+            for(int j=i+1;j<n;j++) {
+                if(j-i != 1) isPal[i][j] = (s[i] == s[j]) && isPal[i+1][j-1];
+                else isPal[i][j] = (s[i] == s[j]);
+            }
+        }
+
         vector<int> dp(n+1, 0);
         
         for(int i=n-1;i>=0;i--) {
@@ -18,7 +20,7 @@ public:
             dp[i] = 1e9;
             for(int j=i;j<n;j++) {
                 temp += s[j];
-                if(isPalindrome(temp)) dp[i] = min(dp[i], 1 + dp[j+1]);
+                if(isPal[i][j]) dp[i] = min(dp[i], 1 + dp[j+1]);
             }
         }
 
