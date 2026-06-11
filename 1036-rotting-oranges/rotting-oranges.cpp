ae2b86b217beff5631be46501bdf156f2046d8c2
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-11 23:30:09 +0530

    Time: 4 ms (15.35%) | Memory: 17.1 MB (39.52%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,45 +2,47 @@ class Solution {
 public:
     int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
-    void bfs(vector<vector<int>>&grid, vector<vector<int>>&vis, int &time) {
-        int n=grid.size(), m=grid[0].size();
-        queue<pair<pair<int,int>, int>> q;
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(grid[i][j]==2) {
-                    q.push({{i, j}, 0});
-                    vis[i][j]=2;
+                if(grid[i][j] == 2) {
+                    vis[i][j] = 1;
+                    q.push({i, j});
                 }
             }
         }
 
+        int time = 0;
         while(!q.empty()) {
-            auto node = q.front();
-            auto [x, y] = node.first;
-            time = max(time, node.second);
-            q.pop();
-            for(int i=0;i<4;i++) {
-                int nr=x+dx[i], nc=y+dy[i];
-                if(nr>=0 && nc>=0 && nr<n && nc<m) {
-                    if(grid[nr][nc]==1 && !vis[nr][nc]) {
-                        vis[nr][nc]=2;
-                        q.push({{nr, nc}, node.second+1});
+            time++;
+            int sz = q.size();
+            for(int i=0;i<sz;i++) {
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop();
+                for(int j=0;j<4;j++) {
+                    int nr = r + dx[j], nc = c + dy[j];
+                    if(nr>=0 && nc>=0 && nr<n && nc<m) {
+                        if(!vis[nr][nc] && grid[nr][nc] == 1) {
+                            grid[nr][nc] = 2;
+                            vis[nr][nc] = 1;
+                            q.push({nr, nc});
+                        }
                     }
                 }
             }
         }
-    }
 
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size(), m=grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m));
-        int time=0;
-        bfs(grid, vis, time);
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(grid[i][j]==1 && vis[i][j]==0) return -1; 
+                if(grid[i][j] == 1) return -1;
             }
         }
-        return time;
+
+        return max(0, time - 1);
     }
 };
