396e2c3009c40e795b81c8e912c57b47dd73ef97
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-12 01:33:07 +0530

    Time: 62 ms (11.41%) | Memory: 49.8 MB (20.13%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
+    int f(int i, int amt, vector<int>&coins, vector<vector<int>>&dp) {
+        int n = coins.size();
+        if(amt == 0) return 0;
+        if(i == n) return 1e9;
+        if(dp[i][amt] != -1) return dp[i][amt];
+        int notTake = f(i+1, amt, coins, dp);
+        int take = INT_MAX;
+        if(amt >= coins[i]) {
+            take = 1 + f(i, amt - coins[i], coins, dp);
+        }
+
+        return dp[i][amt] = min(take, notTake);
+    }
 
     int coinChange(vector<int>& coins, int amt) {
         int n = coins.size();
-        vector<int> prev(amt+1, 1e9), curr(amt+1, 1e9);
-        
-        for(int i=0;i<=amt;i++) {
-            if(i % coins[0] == 0) prev[i] = i/coins[0];
-        }
-
-        for(int i=1;i<n;i++) {
-            for(int j=0;j<=amt;j++) {
-                int notTake = 0 + prev[j];
-                int take = INT_MAX;
-                if(coins[i] <= j) {
-                    take = 1 + curr[j - coins[i]];
-                }
-                curr[j] = min(take, notTake);
-            }
-            prev = curr;
-        }
-
-        if(prev[amt] != 1e9) return prev[amt];
-        return -1;
+        vector<vector<int>> dp(n, vector<int>(amt+1, -1));
+        int ans = f(0, amt, coins, dp);
+        return (ans != 1e9 ? ans : -1);
     }
 };
