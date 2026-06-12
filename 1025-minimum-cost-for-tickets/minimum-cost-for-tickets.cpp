2b7afad1aa1fd18b2c3c27f3eaf8eb4b593c4ed5
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-12 13:59:05 +0530

    Time: 22 ms (5.15%) | Memory: 23.3 MB (5.43%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int f(int i, int ub, vector<int>&days, vector<int>&costs, vector<vector<int>>&dp) {
-        int n = days.size();
-        if(i == n) return 0;
-        if(dp[i][ub] != -1) return dp[i][ub];
-        
-        int notBuy = 1e9;
-        if(ub >= days[i]) {
-            notBuy = 0 + f(i+1, ub, days, costs, dp);
-        }
-
-        int buy = costs[0] + f(i+1, max(ub, days[i]), days, costs, dp);
-        buy = min(buy, costs[1] + f(i+1, max(ub, days[i]+6), days, costs, dp));
-        buy = min(buy, costs[2] + f(i+1, max(ub, days[i]+29), days, costs, dp));
-
-        return dp[i][ub] = min(buy, notBuy);
-    }
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<vector<int>> dp(n, vector<int>(396, -1));
-        return f(0, 0, days, costs, dp);
+        vector<vector<int>> dp(n+1, vector<int>(396, 0));
+
+        for(int i=n-1;i>=0;i--) {
+            for(int j=365;j>=0;j--) {
+                int notBuy = 1e9;
+                if(j >= days[i]) notBuy = dp[i+1][j];
+                int buy = costs[0] + dp[i+1][max(j, days[i])];
+                buy = min(buy, costs[1] + dp[i+1][max(j, days[i]+6)]);
+                buy = min(buy, costs[2] + dp[i+1][max(j, days[i]+29)]);
+                dp[i][j] = min(buy, notBuy);
+            }
+        }
+
+        return dp[0][0];
     }
 };
