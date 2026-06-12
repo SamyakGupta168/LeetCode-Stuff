6ee8561891a0124d492212f43737b0b47df0fe0d
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-12 19:32:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (10.34%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-    bool bfs(int src, vector<vector<int>>&graph, vector<int>&color) {
-        color[src]=0;
+    bool bfs(int src, vector<int>&vis, vector<vector<int>>&graph, vector<int>&color) {
         queue<int> q;
         q.push(src);
+        vis[src] = 1;
+        color[src] = 0;
         while(!q.empty()) {
             int node = q.front();
+            int parentColor = color[node];
             q.pop();
             for(auto it : graph[node]) {
-                if(color[it]==-1) {
-                    color[it] = 1 - color[node];
+                if(!vis[it]) {
+                    vis[it] = 1;
+                    color[it] = 1 - parentColor;
                     q.push(it);
-                } else if(color[it]==color[node]) {
-                    return false;
-                }
+                } else if(parentColor == color[it]) return false;
             }
         }
-        return true;
-    }
 
-    bool dfs(auto src, vector<vector<int>>&graph, vector<int>&color) {
-        int node = src.first;
-        int parentColor = src.second;
-        if(parentColor==-1) color[node] = 0;
-        else color[node] = 1 - parentColor;
-        for(auto it : graph[node]) {
-            if(color[it]==-1) {
-                if(!dfs(make_pair(it, color[node]), graph, color)) return false;
-            } else if(color[it]==color[node]) {
-                return false;
-            }
-        }
         return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> color(n, -1);
+        vector<int> vis(n, 0), color(n, -1);
         for(int i=0;i<n;i++) {
-            if(color[i]==-1) {
-                // if(!bfs(i, graph, color)) return false;
-                if(!dfs(make_pair(i, -1), graph, color)) return false;
+            if(!vis[i]) {
+                if(!bfs(i, vis, graph, color)) return false;
             }
         }
         return true;
