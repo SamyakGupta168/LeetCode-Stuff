06ae9db9f05dc7d09f503ee7052891fc9ebb6e93
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-12 14:12:59 +0530

    Time: 11 ms (5.15%) | Memory: 12.6 MB (94.65%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -3,19 +3,17 @@ public:
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<vector<int>> dp(n+1, vector<int>(396, 0));
+        vector<int> dp(396, 0);
 
         for(int i=n-1;i>=0;i--) {
-            for(int j=365;j>=0;j--) {
-                int notBuy = 1e9;
-                if(j >= days[i]) notBuy = dp[i+1][j];
-                int buy = costs[0] + dp[i+1][max(j, days[i])];
-                buy = min(buy, costs[1] + dp[i+1][max(j, days[i]+6)]);
-                buy = min(buy, costs[2] + dp[i+1][max(j, days[i]+29)]);
-                dp[i][j] = min(buy, notBuy);
+            for(int j=0;j<=365;j++) {
+                if(j < days[i]) dp[j] = 1e9;
+                dp[j] = min(dp[j], costs[0] + dp[max(j, days[i])]);
+                dp[j] = min(dp[j], costs[1] + dp[max(j, days[i]+6)]);
+                dp[j] = min(dp[j], costs[2] + dp[max(j, days[i]+29)]);
             }
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 };
