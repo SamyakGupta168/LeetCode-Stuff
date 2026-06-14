1dd85bb786da140674050e53a81cecccae6c66c3
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-14 19:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (48.89%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int f(int i, string &s, vector<int>&dp) {
-        int n = s.size();
-        if(i >= n) return 1;
-        if(dp[i] != -1) return dp[i];
-
-        int ways = 0;
-        int x1 = s[i] - '0';
-        if(x1 == 0) return 0;
-        ways += 1*f(i+1, s, dp);
-
-        if(i+1 < n) {
-            int x2 = s[i+1] - '0';
-            int num = x1*10 + x2;
-            if(num <= 26) {
-                ways += 1*f(i+2, s, dp);
-            }
-        }
-
-        return dp[i] = ways;
-    }
 
     int numDecodings(string s) {
-        vector<int> dp((int)s.size(), -1);
-        return f(0, s, dp);
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        
+        for(int i=n-1;i>=0;i--) {
+            int ways = 0;
+            int x1 = s[i] - '0';
+            if(x1 == 0) {
+                dp[i] = 0;
+                continue;
+            }
+            ways += 1*dp[i+1];
+
+            if(i+1 < n) {
+                int x2 = s[i+1] - '0';
+                int num = x1*10 + x2;
+                if(num <= 26) {
+                    ways += 1*dp[i+2];
+                }
+            }
+
+            dp[i] = ways;
+        }
+
+        return dp[0];
     }
 };
