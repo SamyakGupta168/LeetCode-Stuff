9af9bdf56b632d89d7724f1f5950f8260c7a21d9
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-16 17:12:49 +0530

    Time: 8 ms (60.40%) | Memory: 25.3 MB (30.57%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int f(int i, int tar, vector<int>&nums, map<int,map<int,int>> &dp) {
-        if(i < 0) {
-            return tar == 0;
+    using ll = long long;
+
+    ll f(int i, int k, vector<int>&nums, vector<vector<ll>>&dp) {
+        if(i == 0) {
+            if(k == 0) {
+                if(nums[0] == 0) return 2;
+                return 1;
+            } else if(k == nums[0]) return 1;
+            return 0;
         }
-        if(dp[i].count(tar)) return dp[i][tar];
-        int ways = f(i-1, tar - nums[i], nums, dp);
-        ways += f(i-1, tar + nums[i], nums, dp);
-        return dp[i][tar] = ways;
+        if(dp[i][k] != -1) return dp[i][k];
+
+        ll notTake = f(i-1, k, nums, dp);
+        ll take = 0;
+        if(k >= nums[i]) take = f(i-1, k - nums[i], nums, dp);
+        return dp[i][k] = take + notTake;
     }
 
     int findTargetSumWays(vector<int>& nums, int tar) {
         int n = nums.size();
-        map<int,map<int,int>> dp;
-        return f(n-1, tar, nums, dp);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(abs(tar) > sum) return 0;
+        vector<vector<ll>> dp(n, vector<ll>(sum+1, -1));
+        int k = (sum - tar);
+        if(k < 0 || k%2 == 1) return 0;
+        return (int)f(n-1, k/2, nums, dp);
     }
 };
