1fb8462a8189a06353b72c440b1c7faf2a8c79c5
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-16 22:35:18 +0530

    Time: 1412 ms (40.27%) | Memory: 30.4 MB (19.49%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -8,22 +8,18 @@ public:
         return true;
     }
 
-    int f(int i, int j, string &s, vector<vector<int>>&dp) {
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        string temp = "";
-        int mini = INT_MAX;
-        for(int k=i;k<=j;k++) {
-            temp += s[k];
-            if(isPalindrome(temp)) mini = min(mini, 1 + f(k+1, j, s, dp));
-        }
-        return dp[i][j] = mini;
-    }
-
     int minCut(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(0, n-1, s, dp) - 1;
+        vector<int> dp(n+1, 0);
+        for(int i=n-1;i>=0;i--) {
+            dp[i] = 1e9;
+            string temp;
+            for(int j=i;j<n;j++) {
+                temp += s[j];
+                if(isPalindrome(temp)) dp[i] = min(dp[i], 1 + dp[j+1]);
+            }
+        }
+
+        return dp[0] - 1;
     }
 };
