3302d0b73b26ea3e819f986981686988e036b57a
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-18 17:03:14 +0530

    Time: 187 ms (66.88%) | Memory: 115.5 MB (63.95%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,7 +1,6 @@
 class DisjointSet {
     public:
     vector<int> rank, parent, size;
-    
     DisjointSet(int n) {
         rank.resize(n+1, 0);
         parent.resize(n+1);
@@ -46,60 +45,56 @@ class DisjointSet {
 };
 
 class Solution {
-  public:
+public:
     int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
-    
+
     int largestIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         DisjointSet ds(n*n);
-        
-        for(int r=0;r<n;r++) {
-            for(int c=0;c<n;c++) {
-                if(grid[r][c] == 0) continue;
-                for(int k=0;k<4;k++) {
-                    int nr = r+dx[k], nc = c+dy[k];
-                    if(nr>=0 && nc>=0 && nr<n && nc<n) {
-                        if(grid[nr][nc] == 1) {
-                            int node = r*n + c;
-                            int adjNode = nr*n + nc;
-                            ds.unionBySize(node, adjNode);
-                        }
-                    }
-                }
-            }
-        }
-        
         int ans = 0;
-        for(int r=0;r<n;r++) {
-            for(int c=0;c<n;c++) {
-                if(grid[r][c] == 1) continue;
-                set<int> ultimate_parents;
-                for(int k=0;k<4;k++) {
-                    int nr = r+dx[k], nc = c+dy[k];
-                    if(nr>=0 && nc>=0 && nr<n && nc<n) {
-                        if(grid[nr][nc] == 1) {
-                            int adjNode = nr*n + nc;
-                            ultimate_parents.insert(ds.findUPar(adjNode));
-                        }
-                    }
-                }
-                
-                int cnt = 1;
-                for(auto parent : ultimate_parents) {
-                    cnt += ds.size[parent];
-                }
-                
-                ans = max(ans, cnt);
-            }
-        }
-        
+
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
-                ans = max(ans, ds.size[ds.findUPar(i*n + j)]);
+                if(grid[i][j] == 1) {
+                    int node = n*i + j;
+                    for(int k=0;k<4;k++) {
+                        int nr = i + dx[k], nc = j + dy[k];
+                        if(nr >= 0 && nc >= 0 && nr < n && nc < n) {
+                            if(grid[nr][nc] == 1) {
+                                int adjNode = nr*n + nc;
+                                ds.unionBySize(node, adjNode);
+                            }
+                        }
+                    }
+                    ans = max(ans, ds.size[ds.findUPar(node)]); 
+                }
             }
         }
-        
+
+
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == 0) {
+                    set<int> st;
+                    for(int k=0;k<4;k++) {
+                        int nr = i + dx[k], nc = j + dy[k];
+                        if(nr >= 0 && nc >= 0 && nr < n && nc < n) {
+                            if(grid[nr][nc] == 1) {
+                                int adjNode = nr*n + nc;
+                                st.insert(ds.findUPar(adjNode));
+                            }
+                        } 
+                    }
+
+                    int cnt = 1;
+                    for(auto x : st) cnt += ds.size[x];
+                    ans = max(ans, cnt);
+                }
+            }
+        }
+
         return ans;
     }
 };
