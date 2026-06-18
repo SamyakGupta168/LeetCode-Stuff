473048683e2eefaa11a38ada112fff56f924ebff
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-18 19:40:57 +0530

    Time: 35 ms (40.24%) | Memory: 28.3 MB (9.78%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,6 +1,6 @@
 class DisjointSet {
-    vector<int> rank, parent, size;
     public:
+    vector<int> rank, parent, size;
     DisjointSet(int n) {
         rank.resize(n+1, 0);
         parent.resize(n+1);
@@ -48,28 +48,44 @@ class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        int maxRow = 0, maxCol = 0;
-        for(auto it : stones) {
-            maxRow = max(maxRow, it[0]);
-            maxCol = max(maxCol, it[1]);
+        map<pair<int,int>, int> node;
+        map<int, vector<int>> row, col;
+        int nodeNo = 0;
+        for(auto &v : stones) {
+            node[{v[0], v[1]}] = nodeNo;
+            nodeNo++;
+            row[v[1]].push_back(v[0]);
+            col[v[0]].push_back(v[1]);
         }
-        
-        DisjointSet ds(maxRow + maxCol + 2);
-        unordered_map<int,int> stoneNodes;
-        
-        for(auto it : stones) {
-            int nodeRow = it[0];
-            int nodeCol = it[1] + maxRow + 1;
-            ds.unionBySize(nodeRow, nodeCol);
-            stoneNodes[nodeRow] = 1;
-            stoneNodes[nodeCol] = 1;
+
+        DisjointSet ds(n);
+        for(auto p : row) {
+            int c = p.first;
+            int prevRow = -1;
+            for(auto r : p.second) {
+                if(prevRow != -1) {
+                    ds.unionBySize(node[{prevRow, c}], node[{r, c}]);
+                }
+                prevRow = r;
+            }
         }
-        
-        int cnt = 0;
-        for(auto it : stoneNodes) {
-            if(ds.findUPar(it.first) == it.first) cnt++;
+
+        for(auto p : col) {
+            int r = p.first;
+            int prevCol = -1;
+            for(auto c : p.second) {
+                if(prevCol != -1) {
+                    ds.unionBySize(node[{r, prevCol}], node[{r, c}]);
+                }
+                prevCol = c;
+            }
         }
-        
-        return n - cnt;
+
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            if(ds.parent[i] == i) ans += ds.size[i] - 1;
+        }
+
+        return ans;
     }
 };
