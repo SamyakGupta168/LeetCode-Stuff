e677448b2d618c1350d3959540833891ae92d46b
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-24 02:37:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (85.15%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -4,17 +4,21 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return 0;
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        dp[0][0] = 1;
-
+        // vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<int> prev(m, 0), curr(m, 0);
+        prev[0] = 1;
+        curr[0] = 1;
+        
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 if(i == 0 && j == 0) continue;
-                if(i-1 >= 0 && grid[i-1][j] == 0) dp[i][j] += dp[i-1][j];
-                if(j-1 >= 0 && grid[i][j-1] == 0) dp[i][j] += dp[i][j-1];
+                curr[j] = 0;
+                if(i-1 >= 0 && grid[i-1][j] == 0) curr[j] += prev[j];
+                if(j-1 >= 0 && grid[i][j-1] == 0) curr[j] += curr[j-1];
             }
+            prev = curr;
         }
         
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
