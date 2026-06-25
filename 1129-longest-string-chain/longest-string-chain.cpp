f803ea31b4d2e51cb0085ab8aa6659720dc089d2
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-25 11:55:09 +0530

    Time: 164 ms (32.36%) | Memory: 106.1 MB (27.33%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     bool check(string &s1, string &s2) {
-        if(s1.size() + 1 != s2.size()) return false;
-        int ind = 0;
-        for(auto ch : s2) {
-            if(ind == s1.size()) break;
-            if(ch == s1[ind]) ind++;
+        if(s1.size() != s2.size() + 1) return false;
+        int i1 = 0, i2 = 0;
+        while(i1 < s1.size() && i2 < s2.size()) {
+            if(s1[i1] == s2[i2]) i2++;
+            i1++;
         }
+        return (i2 == s2.size());
+    }
 
-        return (ind == s1.size());
+    int f(int i, int prev, vector<string>&words, vector<vector<int>>&dp) {
+        int n = words.size();
+        if(i == n+1) return 0;
+        if(dp[i][prev] != -1) return dp[i][prev];
+
+        int take = 0;
+        if(prev == 0 || check(words[i-1], words[prev-1])) {
+            take = 1 + f(i+1, i, words, dp);
+        }
+        int notTake = f(i+1, prev, words, dp);
+        return dp[i][prev] = max(take, notTake);
     }
 
     int longestStrChain(vector<string>& words) {
         int n = words.size();
         sort(words.begin(), words.end(), [&] (auto &s1, auto &s2) -> bool {
-            return (s1.size() < s2.size());
+            return s1.size() < s2.size();
         });
 
-        vector<int> dp(n, 1);
-        for(int i=0;i<n;i++) {
-            for(int prev=0;prev<i;prev++) {
-                if(check(words[prev], words[i])) dp[i] = max(dp[i], 1 + dp[prev]);
-            }
-        }
-
-        return *max_element(dp.begin(), dp.end());
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return f(1, 0, words, dp);
     }
 };
