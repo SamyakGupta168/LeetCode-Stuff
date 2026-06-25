ff7ecd4619e7405fa3e33134f88e7e9d23a6cc3f
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-25 11:14:40 +0530

    Time: 6 ms (29.58%) | Memory: 17.1 MB (42.75%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,24 +1,5 @@
 class Solution {
 public:
-    int f(int i, int flag, vector<pair<int,int>>&a, vector<vector<int>>&dp) {
-        int n = a.size();
-        if(i == n) return 0;
-        if(dp[i][flag] != -1) return dp[i][flag];
-
-        int maxi = 0;
-        if(flag) {
-            maxi = max(maxi, a[i].second*a[i].first + f(i+1, 0, a, dp));
-            maxi = max(maxi, f(i+1, 1, a, dp));
-        } else {
-            maxi = max(maxi, f(i+1, 1, a, dp));
-            if(a[i].first != a[i-1].first + 1) {
-                maxi = max(maxi, a[i].second*a[i].first + f(i+1, 0, a, dp));
-            }
-        }
-
-        return dp[i][flag] = maxi;
-    }
-
     int deleteAndEarn(vector<int>& nums) {
         map<int,int> mp;
         for(auto x : nums) mp[x]++;
@@ -29,18 +10,19 @@ public:
         }
 
         int n = a.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        vector<int> next(2, 0), curr(2, 0);
 
         for(int i=n-1;i>=0;i--) {
-            dp[i][1] = max(dp[i][1], a[i].second * a[i].first + dp[i+1][0]);
-            dp[i][1] = max(dp[i][1], dp[i+1][1]);
+            curr[1] = max(curr[1], a[i].second * a[i].first + next[0]);
+            curr[1] = max(curr[1], next[1]);
 
-            dp[i][0] = max(dp[i][0], dp[i+1][1]);
+            curr[0] = max(curr[0], next[1]);
             if((i == 0) || (a[i].first != a[i-1].first + 1)) {
-                dp[i][0] = max(dp[i][0], a[i].second * a[i].first + dp[i+1][0]);
+                curr[0] = max(curr[0], a[i].second * a[i].first + next[0]);
             }
+            next = curr;
         }
 
-        return dp[0][1];
+        return next[1];
     }
 };
