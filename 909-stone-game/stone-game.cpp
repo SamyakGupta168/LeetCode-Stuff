7b18333061120a08fb93cf0b537e7395761285fe
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-25 12:57:45 +0530

    Time: 295 ms (5.03%) | Memory: 119.6 MB (5.03%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int f(int i, int j, int flag, vector<vector<vector<int>>>&dp, vector<int>&piles) {
-        if(i > j) return 0;
-        if(dp[i][j][flag] != -1) return dp[i][j][flag];
-
-        int maxi = 0;
-        if(flag) {
-            maxi = max(maxi, piles[i] + f(i+1, j, 0, dp, piles));
-            maxi = max(maxi, piles[j] + f(i, j-1, 0, dp, piles));
-        } else {
-            maxi = max(maxi, -piles[i] + f(i+1, j, 1, dp, piles));
-            maxi = max(maxi, -piles[j] + f(i, j-1, 1, dp, piles));
-        }
-        return dp[i][j][flag] = maxi;
-    }
 
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, -1)));
-        int maxScore = f(0, n-1, 1, dp, piles);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(2, 0)));
 
-        return (maxScore > 0);
+        for(int i=0;i<n;i++) {
+            dp[i][i][1] = piles[i];
+            dp[i][i][0] = -piles[i];
+        }
+
+        for(int i=n-1;i>=0;i--) {
+            for(int j=i+1;j<n;j++) {
+                dp[i][j][1] = max(dp[i][j][1], piles[i] + dp[i+1][j][0]);
+                dp[i][j][1] = max(dp[i][j][1], piles[j] + dp[i][j-1][0]);
+                dp[i][j][0] = max(dp[i][j][0], -piles[i] + dp[i+1][j][1]);
+                dp[i][j][0] = max(dp[i][j][0], -piles[j] + dp[i][j-1][1]);
+            }
+        }
+
+        return (dp[0][n-1][1] > 0);
     }
 };
