9d2bb77c28beb5f6c51fa4007f94147872b8d59b
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-25 17:10:09 +0530

    Time: 300 ms (5.03%) | Memory: 119.3 MB (5.03%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -12,10 +12,12 @@ public:
 
         for(int i=n-1;i>=0;i--) {
             for(int j=i+1;j<n;j++) {
+                dp[i][j][1] = INT_MIN, dp[i][j][0] = INT_MAX;
+
                 dp[i][j][1] = max(dp[i][j][1], piles[i] + dp[i+1][j][0]);
                 dp[i][j][1] = max(dp[i][j][1], piles[j] + dp[i][j-1][0]);
-                dp[i][j][0] = max(dp[i][j][0], -piles[i] + dp[i+1][j][1]);
-                dp[i][j][0] = max(dp[i][j][0], -piles[j] + dp[i][j-1][1]);
+                dp[i][j][0] = min(dp[i][j][0], -piles[i] + dp[i+1][j][1]);
+                dp[i][j][0] = min(dp[i][j][0], -piles[j] + dp[i][j-1][1]);
             }
         }
 
