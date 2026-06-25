699a1bf634181e4fe0f2b32583883474cf7fe460
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-25 19:01:44 +0530

    Time: 9 ms (27.60%) | Memory: 19.7 MB (18.33%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -3,24 +3,18 @@ public:
 
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, 0)));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
         for(int i=0;i<n;i++) {
-            dp[i][i][1] = piles[i];
-            dp[i][i][0] = -piles[i];
+            dp[i][i] = piles[i];
         }
 
         for(int i=n-1;i>=0;i--) {
             for(int j=i+1;j<n;j++) {
-                dp[i][j][1] = INT_MIN, dp[i][j][0] = INT_MAX;
-
-                dp[i][j][1] = max(dp[i][j][1], piles[i] + dp[i+1][j][0]);
-                dp[i][j][1] = max(dp[i][j][1], piles[j] + dp[i][j-1][0]);
-                dp[i][j][0] = min(dp[i][j][0], -piles[i] + dp[i+1][j][1]);
-                dp[i][j][0] = min(dp[i][j][0], -piles[j] + dp[i][j-1][1]);
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
             }
         }
 
-        return (dp[0][n-1][1] > 0);
+        return (dp[0][n-1] >= 0); 
     }
 };
