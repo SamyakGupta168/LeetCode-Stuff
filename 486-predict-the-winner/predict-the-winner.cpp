2985ab1a640f72b1e6de8deb069f0b4aeff47d79
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-25 18:58:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (14.43%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, 0)));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
         for(int i=0;i<n;i++) {
-            dp[i][i][1] = nums[i];
-            dp[i][i][0] = -nums[i];
+            dp[i][i] = nums[i];
         }
 
         for(int i=n-1;i>=0;i--) {
             for(int j=i+1;j<n;j++) {
-                dp[i][j][1] = INT_MIN, dp[i][j][0] = INT_MAX;
-                
-                dp[i][j][1] = max(dp[i][j][1], nums[i] + dp[i+1][j][0]);
-                dp[i][j][1] = max(dp[i][j][1], nums[j] + dp[i][j-1][0]);
-                dp[i][j][0] = min(dp[i][j][0], -nums[i] + dp[i+1][j][1]);
-                dp[i][j][0] = min(dp[i][j][0], -nums[j] + dp[i][j-1][1]);
+                dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1]);
             }
         }
 
-        return (dp[0][n-1][1] >= 0);        
+        return (dp[0][n-1] >= 0);        
     }
 };
