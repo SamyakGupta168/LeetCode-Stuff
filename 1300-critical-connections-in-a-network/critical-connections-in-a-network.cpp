811a72e0177ce3799d12e902c864c74f5f74a5d8
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-06-30 15:01:10 +0530

    Time: 146 ms (79.18%) | Memory: 168.9 MB (88.09%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    void dfs(int node, int parent, vector<vector<int>>&adj, vector<int>&time, vector<int>&low, int &t, vector<vector<int>>&ans) {
+    void dfs(int node, vector<int>&time, vector<int>&low, vector<vector<int>>&adj, int t, vector<vector<int>>&edges, int parent) {
         t++;
         time[node] = t;
-        low[node] = t;
+        low[node] = time[node];
+
         for(auto it : adj[node]) {
             if(!time[it]) {
-                dfs(it, node, adj, time, low, t, ans);
+                dfs(it, time, low, adj, t, edges, node);
                 low[node] = min(low[node], low[it]);
                 if(time[node] < low[it]) {
-                    ans.push_back({node, it});
+                    edges.push_back({node, it});
                 }
             } else if(it != parent) {
                 low[node] = min(low[node], low[it]);
@@ -17,20 +18,17 @@ public:
         }
     }
 
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& conn) {
         vector<vector<int>> adj(n);
-        for(auto &v : connections) {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
+        for(auto &e : conn) {
+            int u = e[0], v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
-        vector<vector<int>> ans;
         vector<int> time(n, 0), low(n, INT_MAX);
-        int t = 0;
-        for(int i=0;i<n;i++) {
-            if(!time[i]) dfs(i, -1, adj, time, low, t, ans);
-        }
-
-        return ans;
+        vector<vector<int>> edges;
+        dfs(0, time, low, adj, 0, edges, -1);
+        return edges;
     }
 };
