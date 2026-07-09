9373d1f34fd99a3f90d7050a99f4e1724bb5319b
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-09 14:59:32 +0530

    Time: 924 ms (29.66%) | Memory: 289.6 MB (6.45%) - LeetSync

---
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    using ll = long long;
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        unordered_map<ll, unordered_map<ll,ll>> dp;
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                ll diff = (ll)nums[j] - nums[i];
+                int cnt = dp[i][diff];
+                ans += cnt;
+                dp[j][diff] += cnt+1;
+            }
+        }
+
+        return ans;
+    }
+};
