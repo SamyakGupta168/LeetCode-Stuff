ddbf2b9ec4e755ae65ed3e3241d3d81313278148
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-10 11:20:50 +0530

    Time: 10 ms (44.41%) | Memory: 9.9 MB (97.95%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
@@ -7,32 +7,40 @@ public:
 
     vector<int> pathsWithMaxScore(vector<string>& board) {
         int n = board.size(), m = board[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        vector<vector<int>> ways(n, vector<int>(m, 0));
-        dp[n-1][m-1] = 0;
-        ways[n-1][m-1] = 1;
+        vector<int> next(m, -1), curr(m, -1);
+        vector<int> currWays(m, 0), nextWays(m, 0);
+        next[m-1] = 0;
+        nextWays[m-1] = 1;
         for(int i=n-1;i>=0;i--) {
             for(int j=m-1;j>=0;j--) {
-                if(i == n-1 && j == m-1) continue;
+                if(i == n-1 && j == m-1) {
+                    curr[j] = 0;
+                    currWays[j] = 1;
+                    continue;
+                }
                 if(board[i][j] == 'X') continue;
                 int maxVal = -1;
-                if(isValid(i, n, j+1, m)) maxVal = max(maxVal, dp[i][j+1]);
-                if(isValid(i+1, n, j, m)) maxVal = max(maxVal, dp[i+1][j]);
-                if(isValid(i+1, n, j+1, m)) maxVal = max(maxVal, dp[i+1][j+1]);
+                if(isValid(i, n, j+1, m)) maxVal = max(maxVal, curr[j+1]);
+                if(isValid(i+1, n, j, m)) maxVal = max(maxVal, next[j]);
+                if(isValid(i+1, n, j+1, m)) maxVal = max(maxVal, next[j+1]);
                 if(maxVal != -1) {
-                    if(i == 0 && j == 0) dp[i][j] = maxVal;
-                    else dp[i][j] = (board[i][j] - '0') + maxVal;
-                    if(isValid(i, n, j+1, m) && dp[i][j+1] == maxVal) ways[i][j] += ways[i][j+1];
-                    ways[i][j] %= mod;
-                    if(isValid(i+1, n, j, m) && dp[i+1][j] == maxVal) ways[i][j] += ways[i+1][j];
-                    ways[i][j] %= mod;
-                    if(isValid(i+1, n, j+1, m) && dp[i+1][j+1] == maxVal) ways[i][j] += ways[i+1][j+1];
-                    ways[i][j] %= mod;
+                    if(i == 0 && j == 0) curr[j] = maxVal;
+                    else curr[j] = (board[i][j] - '0') + maxVal;
+                    if(isValid(i, n, j+1, m) && curr[j+1] == maxVal) currWays[j] += currWays[j+1];
+                    currWays[j] %= mod;
+                    if(isValid(i+1, n, j, m) && next[j] == maxVal) currWays[j] += nextWays[j];
+                    currWays[j] %= mod;
+                    if(isValid(i+1, n, j+1, m) && next[j+1] == maxVal) currWays[j] += nextWays[j+1];
+                    currWays[j] %= mod;
                 }
             }
+            next = curr;
+            nextWays = currWays;
+            fill(curr.begin(), curr.end(), -1);
+            fill(currWays.begin(), currWays.end(), 0);
         }
 
-        if(dp[0][0] == -1) return {0, 0};
-        return {dp[0][0], ways[0][0]};
+        if(next[0] == -1) return {0, 0};
+        return {next[0], nextWays[0]};
     }
 };
