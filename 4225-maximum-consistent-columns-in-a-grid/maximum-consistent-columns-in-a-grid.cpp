b6f6d0238392fc0a77a1537a04f17b7b194e0018
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-12 15:46:38 +0530

    Time: 153 ms (21.89%) | Memory: 102.6 MB (58.66%) - LeetSync

--- 4225-maximum-consistent-columns-in-a-grid/maximum-consistent-columns-in-a-grid.cpp
+++ 4225-maximum-consistent-columns-in-a-grid/maximum-consistent-columns-in-a-grid.cpp
@@ -7,29 +7,20 @@ public:
         }
         return true;
     }
-
-    int f(int i, vector<vector<int>>&grid, int limit, vector<int>&dp) {
-        if(i < 0) return 0;
-        if(dp[i] != -1) return dp[i];
-
-        int maxi = 1;
-        for(int j=0;j<i;j++) {
-            if(check(i, j, grid, limit)) {
-                maxi = max(maxi, 1 + f(j, grid, limit, dp));
-            }
-        }
-
-        return dp[i] = maxi;
-    }
-
+    
     int maxConsistentColumns(vector<vector<int>>& grid, int limit) {
         int n = grid.size(), m = grid[0].size();
         int ans = 0;
-        vector<int> dp(m, -1);
-        for(int i=0;i<m;i++) {
-            ans = max(ans, f(i, grid, limit, dp));
+        vector<int> dp(m+1, 0);
+        for(int i=1;i<=m;i++) {
+            dp[i] = 1;
+            for(int j=1;j<i;j++) {
+                if(check(i-1, j-1, grid, limit)) {
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                }
+            }
         }
 
-        return ans;
+        return *max_element(dp.begin(), dp.end());
     }
 };
