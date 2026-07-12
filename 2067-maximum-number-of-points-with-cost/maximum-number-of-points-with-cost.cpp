e60d5e6bb450e5c5accacb1ac03174e04ab1fe9e
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-13 00:16:23 +0530

    Time: 16 ms (89.98%) | Memory: 90.2 MB (89.98%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
@@ -3,32 +3,33 @@ public:
     using ll = long long;
     long long maxPoints(vector<vector<int>>& points) {
         int n = points.size(), m = points[0].size();
-        vector<vector<ll>> dp(n, vector<ll>(m, 0));
+        vector<ll> prev(m, 0), cur(m, 0);
         for(int j=0;j<m;j++) {
-            dp[0][j] = points[0][j];
+            prev[j] = points[0][j];
         }
 
-        vector<ll> left(m, dp[0][0]), right(m, dp[0][m-1] - m + 1);
-        for(int j=1;j<m;j++) left[j] = max(left[j-1], dp[0][j] + j);
-        for(int j=m-2;j>=0;j--) right[j] = max(right[j+1], dp[0][j] - j);
+        vector<ll> left(m, prev[0]), right(m, prev[m-1] - m + 1);
+        for(int j=1;j<m;j++) left[j] = max(left[j-1], prev[j] + j);
+        for(int j=m-2;j>=0;j--) right[j] = max(right[j+1], prev[j] - j);
 
         for(int i=1;i<n;i++) {
             for(int j=0;j<m;j++) {
-                dp[i][j] = points[i][j] + max(left[j] - j, right[j] + j);
+                cur[j] = points[i][j] + max(left[j] - j, right[j] + j);
             }
 
-            left[0] = dp[i][0];
-            right[m-1] = dp[i][m-1] - m + 1;
+            left[0] = cur[0];
+            right[m-1] = cur[m-1] - m + 1;
             for(int j=1;j<m;j++) {
                 left[j] = 0;
-                left[j] = max(left[j-1], dp[i][j] + j);
+                left[j] = max(left[j-1], cur[j] + j);
             }
             for(int j=m-2;j>=0;j--) {
                 right[j] = 0;
-                right[j] = max(right[j+1], dp[i][j] - j);
+                right[j] = max(right[j+1], cur[j] - j);
             }
+            prev = cur;
         }
 
-        return *max_element(dp[n-1].begin(), dp[n-1].end());
+        return *max_element(prev.begin(), prev.end());
     }
 };
