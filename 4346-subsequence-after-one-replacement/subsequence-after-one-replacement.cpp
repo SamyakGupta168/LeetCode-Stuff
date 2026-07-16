7620c302ebb5d323e0179df70d3d9afae5cfec5d
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-16 21:57:34 +0530

    Time: 357 ms (5.04%) | Memory: 147.6 MB (5.12%) - LeetSync

--- 4346-subsequence-after-one-replacement/subsequence-after-one-replacement.cpp
+++ 4346-subsequence-after-one-replacement/subsequence-after-one-replacement.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     bool canMakeSubsequence(string s, string t) {
-        int n = t.size();
-        vector<int> prev(2, 0), curr(2, 0);
-        prev[1] = 1;
-        prev[0] = (s[0] == t[0]);
-        for(int i=1;i<n;i++) {
-            int idx1 = prev[1];
-            if(idx1 == s.size()) {
-                curr[1] = idx1;
-            } else if(s[idx1] == t[i]) {
-                curr[1] = idx1 + 1;
-            } else {
-                idx1 = max(idx1, prev[0] + 1);
-                curr[1] = idx1;
-            }
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(m, vector<int>(2, 0));
+        dp[0][0] = (s[0] == t[0]);
+        dp[0][1] = 1;
+        int ans = max(dp[0][0], dp[0][1]);
+        for(int i=1;i<m;i++) {
+            int idx1 = dp[i-1][0];
+            if(idx1 < n && t[i] == s[idx1]) dp[i][0] = 1 + dp[i-1][0];
+            else dp[i][0] = dp[i-1][0];
 
-            int idx2 = prev[0];
-            if(idx2 == s.size()) {
-                curr[0] = idx2;
-            } else if(s[idx2] == t[i]) {
-                curr[0] = idx2 + 1;
-            } else {
-                curr[0] = idx2;
+            if(dp[i-1][0] != n) dp[i][1] = 1 + dp[i-1][0];
+            if(dp[i-1][1] < n ) {
+                int idx2 = dp[i-1][1];
+                if(t[i] == s[idx2]) dp[i][1] = max(dp[i][1], 1 + dp[i-1][1]);
+                else dp[i][1] = max(dp[i][1], dp[i-1][0]);
             }
-
-            prev = curr;
+            ans = max(ans, dp[i][0]);
+            ans = max(ans, dp[i][1]);
         }
-
-        return prev[1] == s.size();
+        cout<<ans<<'\n';
+        return ans == n;
     }
 };
