2de73a3adc13f85f7d2a933aeb661e73cf81256c
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-16 23:53:15 +0530

    Time: 67 ms (5.05%) | Memory: 20.4 MB (16.14%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -4,27 +4,30 @@ public:
     int minRefuelStops(int tar, int start, vector<vector<int>>& st) {
         st.insert(st.begin(), {0, start});
         int n = st.size();
-        vector<vector<ll>> dp(n, vector<ll>(n, -1));
-        dp[0][0] = start;
+        // vector<vector<ll>> dp(n, vector<ll>(n, -1));
+        vector<ll> prev(n, -1), cur(n, -1);
+        prev[0] = start;
         for(int i=1;i<n;i++) {
             for(int j=0;j<i;j++) {
-                if(j>0 && dp[i-1][j-1] != -1) {
-                    if(dp[i-1][j-1] + st[i-1][1] >= st[i][0] - st[i-1][0]) {
-                        dp[i][j] = max(dp[i][j], dp[i-1][j-1] + st[i-1][1] - st[i][0] + st[i-1][0]);
+                if(j>0 && prev[j-1] != -1) {
+                    if(prev[j-1] + st[i-1][1] >= st[i][0] - st[i-1][0]) {
+                        cur[j] = max(cur[j], prev[j-1] + st[i-1][1] - st[i][0] + st[i-1][0]);
                     }
                 }
 
-                if(dp[i-1][j] != -1 && dp[i-1][j] >= st[i][0] - st[i-1][0]) {
-                    dp[i][j] = max(dp[i][j], dp[i-1][j] - st[i][0] + st[i-1][0]);
+                if(prev[j] != -1 && prev[j] >= st[i][0] - st[i-1][0]) {
+                    cur[j] = max(cur[j], prev[j] - st[i][0] + st[i-1][0]);
                 }
             }
+            prev = cur;
+            fill(cur.begin(), cur.end(), -1);
         }
 
         int ans = INT_MAX;
         for(int j=0;j<n;j++) {
-            if(dp[n-1][j] != -1) {
-                if(st[n-1][0] + dp[n-1][j] >= tar) ans = min(ans, j);
-                if(st[n-1][0] + dp[n-1][j] + st[n-1][1] >= tar) ans = min(ans, j+1);
+            if(prev[j] != -1) {
+                if(st[n-1][0] + prev[j] >= tar) ans = min(ans, j);
+                if(st[n-1][0] + prev[j] + st[n-1][1] >= tar) ans = min(ans, j+1);
             }
         }
 
