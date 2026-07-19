c9f2fa772422a2f41b33d818069da1b9e273184d
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-19 23:38:16 +0530

    Time: 19 ms (28.57%) | Memory: 149.1 MB (28.57%) - LeetSync

--- 4335-minimum-adjacent-swaps-to-partition-array/minimum-adjacent-swaps-to-partition-array.cpp
+++ 4335-minimum-adjacent-swaps-to-partition-array/minimum-adjacent-swaps-to-partition-array.cpp
@@ -2,47 +2,26 @@ class Solution {
 public:
     long long mod = 1e9 + 7;
     int minAdjacentSwaps(vector<int>& nums, int a, int b) {
-        int n = nums.size();
-        vector<int> ind1;
-        int ca = 0, cb = 0;
+        int n = nums.size();    
+        long long ans = 0;
+        int p = 0;
         for(int i=0;i<n;i++) {
             if(nums[i] < a) {
-                ca++;
-            }
-            if(nums[i] > b) {
-                cb++;
-                ind1.push_back(i);
+                ans += i - p;
+                ans %= mod;
+                p++;
             }
         }
 
-        long long ans = 0, idx = n-1;
-        while(cb > 0) {
-            ans += idx - ind1.back();
-            ans %= mod;
-            ind1.pop_back();
-            idx--, cb--;
-        }
-
-        vector<int> temp;
-        for(auto x : nums) {
-            if(x <= b) temp.push_back(x);
-        }
-
-        for(auto x : nums) {
-            if(x > b) temp.push_back(x); 
-        }
-
-        vector<int> ind2;
+        int ex = 0;
+        p = n-1;
         for(int i=n-1;i>=0;i--) {
-            if(temp[i] < a) ind2.push_back(i);
-        }
-
-        idx = 0;
-        while(ca > 0) {
-            ans += ind2.back() - idx;
-            ans %= mod;
-            ind2.pop_back();
-            idx++, ca--;
+            if(nums[i] < a) ex++;
+            else if(nums[i] > b) {
+                ans += p - i - ex;
+                ans %= mod;
+                p--;
+            }
         }
 
         return (int)ans;
