a27618ade7586a9465ea376f38729866b3a6e457
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-24 19:50:13 +0530

    Time: 31 ms (28.33%) | Memory: 19.4 MB (36.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,32 +1,80 @@
-class Solution {
-public:
-    int dx[4] = {0, 0, 1, -1};
-    int dy[4] = {1, -1, 0, 0};
-
-    void dfs(int r, int c, vector<vector<int>>&vis, vector<vector<char>>&grid) {
-        int n = grid.size(), m = grid[0].size();
-        vis[r][c] = 1;
-        for(int i=0;i<4;i++) {
-            int nr = r + dx[i], nc = c + dy[i];
-            if(nr>=0 && nc>=0 && nr<n && nc<m) {
-                if(!vis[nr][nc] && grid[nr][nc] == '1') dfs(nr, nc, vis, grid);
-            }
+class DisjointSet {
+    vector<int> rank, parent, size;
+    public:
+    DisjointSet(int n) {
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
+    int findUPar(int node) {
+        if(node == parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if(rank[ulp_u] > rank[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {1, -1, 0, 0};
+
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        int cnt = 0;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(!vis[i][j] && grid[i][j] == '1') {
-                    cnt++;
-                    dfs(i, j, vis, grid);
+        DisjointSet ds(n*m);
+        for(int r=0;r<n;r++) {
+            for(int c=0;c<m;c++) {
+                if(grid[r][c] == '0') continue;
+                int node = r*m + c;
+                for(int i=0;i<4;i++) {
+                    int nr = r + dx[i], nc = c + dy[i];
+                    if(nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                        if(grid[nr][nc] == '1') {
+                            int adjNode = nr*m + nc;
+                            ds.unionBySize(node, adjNode);
+                        }
+                    }
                 }
             }
         }
 
-        return cnt;
+        unordered_set<int> st;
+        for(int node=0;node<n*m;node++) {
+            int parent = ds.findUPar(node);
+            int r = parent / m, c = parent % m;
+            if(grid[r][c] == '1') st.insert(parent);
+        }
+
+        return (int)st.size();
     }
 };
