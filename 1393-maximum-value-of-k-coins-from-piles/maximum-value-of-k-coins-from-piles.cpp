7eaa203343c6b7932920e07695b1cf9bf139e597
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-24 14:15:22 +0530

    Time: 64 ms (99.29%) | Memory: 16 MB (94.67%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
@@ -7,22 +7,20 @@ public:
         }
 
         int n = piles.size();
-        vector<vector<ll>> dp(n, vector<ll>(k+1, 0));
+        vector<ll> dp(k+1, 0);
 
         for(int j=0;j<min((int)piles[0].size(), k);j++) {
-            dp[0][j+1] = piles[0][j];
+            dp[j+1] = piles[0][j];
         }
 
         for(int i=1;i<n;i++) {
-            for(int j=1;j<=k;j++) {
-                ll maxSum = dp[i-1][j];
+            for(int j=k;j>=1;j--) {
                 for(int l=0;l<min((int)piles[i].size(), j);l++) {
-                    maxSum = max(maxSum, piles[i][l] + dp[i-1][j-l-1]);
+                    dp[j] = max(dp[j], piles[i][l] + dp[j-l-1]);
                 }
-                dp[i][j] = maxSum;
             }
         }
 
-        return dp[n-1][k];
+        return dp[k];
     }
 };
