f3ba82b321c5813463d8240c37a75fd8e864306e
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-07-24 14:06:29 +0530

    Time: 89 ms (95.73%) | Memory: 48 MB (7.70%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     using ll = long long;
-    ll f(int i, int k, vector<vector<int>>&piles, vector<vector<ll>>&dp) {
-        if(k == 0) return 0;
-        if(i == 0) {
-            if(piles[0].size() >= k) return piles[0][k-1];
-            return -2e9;
-        }
-        if(dp[i][k] != -1) return dp[i][k];
-        ll maxSum = f(i-1, k, piles, dp);
-        for(int j=0;j<min((int)piles[i].size(), k);j++) {
-            maxSum = max(maxSum, piles[i][j] + f(i-1, k-j-1, piles, dp));
-        }
-        return dp[i][k] = maxSum;
-    }
-
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         for(auto &v : piles) {
             for(int i=1;i<v.size();i++) v[i] += v[i-1];
         }
+
         int n = piles.size();
-        vector<vector<ll>> dp(n, vector<ll>(k+1, -1));
-        return (int)f(n-1, k, piles, dp);
+        vector<vector<ll>> dp(n, vector<ll>(k+1, -2e9));
+
+        for(int i=0;i<n;i++) dp[i][0] = 0;
+
+        for(int j=0;j<min((int)piles[0].size(), k);j++) {
+            dp[0][j+1] = piles[0][j];
+        }
+
+        for(int i=1;i<n;i++) {
+            for(int j=1;j<=k;j++) {
+                ll maxSum = dp[i-1][j];
+                for(int l=0;l<min((int)piles[i].size(), j);l++) {
+                    maxSum = max(maxSum, piles[i][l] + dp[i-1][j-l-1]);
+                }
+                dp[i][j] = maxSum;
+            }
+        }
+
+        return dp[n-1][k];
     }
 };
