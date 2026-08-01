9be93e4d289aae4e649b0cc3f707c60dec564498
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-08-01 12:13:53 +0530

    Time: 27 ms (45.31%) | Memory: 18.2 MB (88.37%) - LeetSync

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int minCost(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dist(n, vector<int> (m, INT_MAX));
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
+        dist[0][0] = 0;
+        pq.push({0, {0, 0}});
+        while(!pq.empty()) {
+            int dis = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
+            pq.pop();
+
+            for(int i=0;i<4;i++) {
+                int nr = r + dx[i], nc = c + dy[i];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                    int d = grid[r][c] == i+1 ? dis : dis + 1;
+                    if(d < dist[nr][nc]) {
+                        dist[nr][nc] = d;
+                        pq.push({dist[nr][nc], {nr, nc}});
+                    }
+                }
+            }
+        }
+
+        return dist[n-1][m-1];
+    }
+};
