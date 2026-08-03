d4998f37a00cc2f15680049ad0a3b3c72e223478
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-08-03 20:37:07 +0530

    Time: 7 ms (27.35%) | Memory: 18.3 MB (37.75%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -8,30 +8,32 @@ public:
             int wt = v[2];
             adj[from].push_back({to, wt});
         }
+        // dist[node][stop] -> minimum cost to reach 'node' with exactly 'stop' stops.
+        vector<vector<int>> dist(n, vector<int>(k+2, INT_MAX));
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        pq.push({0, {0, src}});
+        dist[src][0] = 0;
+        while(!pq.empty()) {
+            int dis = pq.top().first;
+            int stops = pq.top().second.first;
+            int node = pq.top().second.second;
+            pq.pop();
 
-        vector<int> dist(n, 1e9);
-        // priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        queue<pair<int,pair<int,int>>> q;
-        q.push({0, {0, src}});
-        dist[src] = 0;
-        while(!q.empty()) {
-            int stops = q.front().first;
-            int dis=  q.front().second.first;
-            int node = q.front().second.second;
-            q.pop();
+            if(node == dst || stops == k+1) continue;
 
-            if(stops == k+1) continue;
-
-            for(auto it : adj[node]) {
+            for(auto &it : adj[node]) {
                 int adjNode = it.first;
                 int edw = it.second;
-                if(dis + edw < dist[adjNode]) {
-                    dist[adjNode] = dis + edw;
-                    q.push({stops + 1, {dist[adjNode], adjNode}});
+                if(dis + edw < dist[adjNode][stops+1]) {
+                    dist[adjNode][stops+1] = dis + edw;
+                    pq.push({dist[adjNode][stops+1], {stops+1, adjNode}});
                 }
             }
         }
 
-        return (dist[dst] != 1e9 ? dist[dst] : -1);
+        int ans = INT_MAX;
+        for(auto dis : dist[dst]) ans = min(ans, dis);
+
+        return (ans != INT_MAX ? ans : -1); 
     }
 };
