9818471952dbc444d07d64bf69fa86d6c4131cc4
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-08-04 01:34:33 +0530

    Time: 24 ms (68.48%) | Memory: 147.7 MB (19.73%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string stoneGameIII(vector<int>& val) {
+        int n = val.size();
+        vector<vector<int>> dp(2, vector<int>(n+3, 0));
+        for(int i=n-1;i>=0;i--) {
+            dp[0][i] = val[i] - dp[1][i+1];
+            if(i+1 < n) dp[0][i] = max(dp[0][i], val[i] + val[i+1] - dp[1][i+2]);
+            if(i+2 < n) dp[0][i] = max(dp[0][i], val[i] + val[i+1] + val[i+2] - dp[1][i+3]);
+
+            dp[1][i] = val[i] - dp[0][i+1];
+            if(i+1 < n) dp[1][i] = max(dp[1][i], val[i] + val[i+1] - dp[0][i+2]);
+            if(i+2 < n) dp[1][i] = max(dp[1][i], val[i] + val[i+1] + val[i+2] - dp[0][i+3]);
+        }
+
+        if(dp[0][0] > 0) return "Alice";
+        else if(dp[0][0] < 0) return "Bob";
+        return "Tie";
+    }
+};
