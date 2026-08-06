44ca27b0138fd3f18cd80b0b64f64ec0376da3c5
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-08-07 02:03:08 +0530

    Time: 182 ms (50.07%) | Memory: 308.4 MB (43.84%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -1,55 +1,43 @@
-constexpr int MAXN = 100005;
-
 class Solution {
 public:
-    vector<int> remainingMethods(int n, int k,
-                                 vector<vector<int>>& invocations) {
-        vector<vector<int>> edges(n);
-        vector<int> inDegree(n, 0);
+    void dfs1(int node, vector<int>&mark, vector<vector<int>>&adj) {
+        mark[node] = 1;
+        for(auto it : adj[node]) {
+            if(!mark[it]) dfs1(it, mark, adj);
+        }
+    }
 
-        bitset<MAXN> suspicious;
-
-        for (const auto& inv : invocations) {
-            edges[inv[0]].push_back(inv[1]);
-            inDegree[inv[1]]++;
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& in) {
+        vector<vector<int>> adj(n);
+        for(auto &x : in) {
+            int u = x[0], v = x[1];
+            adj[u].push_back(v);
         }
 
-        queue<int> q;
-        q.push(k);
+        vector<int> mark(n, 0);
+        dfs1(k, mark, adj);
 
-        suspicious.set(k);
-
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            for (int v : edges[u]) {
-                inDegree[v]--;
-
-                if (!suspicious.test(v)) {
-                    q.push(v);
-                    suspicious.set(v);
+        bool flag = false;
+        for(int i=0;i<n;i++) {
+            if(!mark[i]) {
+                for(auto it : adj[i]) {
+                    if(mark[it]) {
+                        flag = true;
+                        break;
+                    }
                 }
             }
         }
-
-        bool canRemoveAll = true;
-        vector<int> remaining;
-
-        for (int i = 0; i < n; i++) {
-            if (suspicious.test(i) && inDegree[i] > 0) {
-                canRemoveAll = false;
-                break;
-            } else if (!suspicious.test(i)) {
-                remaining.push_back(i);
+        
+        vector<int> nodes;
+        if(flag) {
+            for(int i=0;i<n;i++) nodes.push_back(i);
+        } else {
+            for(int i=0;i<n;i++) {
+                if(!mark[i]) nodes.push_back(i);
             }
         }
 
-        if (!canRemoveAll) {
-            vector<int> allNodes(n);
-            iota(allNodes.begin(), allNodes.end(), 0);
-            return allNodes;
-        }
-
-        return remaining;
+        return nodes;
     }
 };
