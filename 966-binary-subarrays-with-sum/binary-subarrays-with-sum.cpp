587e230dcca179a21155d0e75050416e89e66eb7
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-08-07 13:04:40 +0530

    Time: 58 ms (5.02%) | Memory: 40.3 MB (7.74%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    int cntOfSubarray(int k, vector<int>&nums, vector<int>&pref) {
-        if(k < 0) return 0;
-        int n = nums.size();
-        int prev = 0, cnt = 0;
-        for(int i=0;i<n;i++) {
-            int idx = lower_bound(pref.begin(), pref.end(), prev + k + 1) - pref.begin();
-            prev = pref[i];
-            cnt += idx - i;
-        }
-        return cnt;
-    }
-
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n = nums.size();
         vector<int> pref(n, nums[0]);
