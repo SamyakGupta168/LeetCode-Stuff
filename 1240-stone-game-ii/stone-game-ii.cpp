963c4fde545345e1609e68a33aaa8ca263f6dd67
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-08-11 00:19:33 +0530

    Time: 35 ms (20.88%) | Memory: 11.1 MB (91.02%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    int dp[2][101][101];
-
-    int f(int person, int i, int m, vector<int>&piles, int dp[][101][101]) {
-        int n = piles.size();
-        if(i >= n) return 0;
-        if(dp[person][i][m] != -1) return dp[person][i][m];
-        int result = (person == 1 ? INT_MIN : INT_MAX);
-        int stones = 0;
-
-        for(int x=1;x<=min(2*m, n-i);x++) {
-            int idx = i + x - 1;
-            stones += piles[idx];
-            if(person == 1) {
-                result = max(result, stones + f(0, idx+1, max(x, m), piles, dp));
-            } else {
-                result = min(result, f(1, idx+1, max(x, m), piles, dp));
-            }
-        }
-
-        return dp[person][i][m] = result;
-    }
+    int dp[2][102][102];
 
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        memset(dp, -1, sizeof(dp));
-        return f(1, 0, 1, piles, dp);
+        memset(dp, 0, sizeof(dp));
+        for(int i=n-1;i>=0;i--) {
+            for(int j=n;j>=1;j--) {
+                dp[1][i][j] = INT_MIN;
+                dp[0][i][j] = INT_MAX;
+                int stones = 0;
+                for(int x=1;x<=min(2*j, n-i);x++) {
+                    int idx = i + x - 1;
+                    stones += piles[idx];
+                    dp[1][i][j] = max(dp[1][i][j], stones + dp[0][idx+1][max(x, j)]);
+                    dp[0][i][j] = min(dp[0][i][j], dp[1][idx+1][max(x, j)]);
+                }
+            }
+        }
+
+        return dp[1][0][1];
     }
 };
