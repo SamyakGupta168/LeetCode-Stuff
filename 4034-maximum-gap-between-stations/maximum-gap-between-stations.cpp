6752574c247bfe045c8afdb5978c931436052597
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-08-16 11:23:34 +0530

    Time: 66 ms (26.99%) | Memory: 72.1 MB (81.11%) - LeetSync

--- 4034-maximum-gap-between-stations/maximum-gap-between-stations.cpp
+++ 4034-maximum-gap-between-stations/maximum-gap-between-stations.cpp
@@ -8,20 +8,18 @@ public:
         while(i < n && j < m) {
             if(skill[i] == stat[j]) {
                 left[i] = j;
-                i++, j++;
-            } else {
-                j++;
+                i++;
             }
+            j++;
         } 
 
         i = n-1, j = m-1;
         while(i >= 0 && j >= 0) {
             if(skill[i] == stat[j]) {
                 right[i] = j;
-                i--, j--;
-            } else {
-                j--;
-            }
+                i--;
+            } 
+            j--;
         }
 
         int mx = -1;
