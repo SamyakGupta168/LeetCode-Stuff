491f1bab2b45dd3788de7c17122917adcad9d1dc
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-08-23 13:00:20 +0530

    Time: 226 ms (36.20%) | Memory: 197.8 MB (55.49%) - LeetSync

---
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+        vector<vector<int>> adj(n);
+        for(auto &e : edges) {
+            int u = e[0]-1, v = e[1]-1;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<vector<int>> dist(n, vector<int>(2, INT_MAX));
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        dist[0][0] = 0;
+        pq.push({0, 0});
+        while(!pq.empty()) {
+            int node = pq.top().second;
+            int dis = pq.top().first;
+            pq.pop();
+
+            for(auto adjNode : adj[node]) {
+                int dd = dis + time;
+                int r = dis / change;
+                if(r & 1) dd += change - (dis % change);
+                if(dd < dist[adjNode][0]) {
+                    dist[adjNode][1] = dist[adjNode][0];
+                    dist[adjNode][0] = dd;
+                    pq.push({dd, adjNode});
+                } else if(dd < dist[adjNode][1] && dd > dist[adjNode][0]) {
+                    dist[adjNode][1] = dd;
+                    pq.push({dd, adjNode});
+                }
+            }
+        }
+
+        return dist[n-1][1];
+    }
+};
