d4bc8ceedd2818c76c2bbfff5218761f09b01a4a
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-09-22 20:09:01 +0530

    Time: 6 ms (87.64%) | Memory: 12.5 MB (95.79%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    int dx[4] = {0, 0, -1, 1};
+    int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
+
     int shortestPath(vector<vector<int>>& grid, int k) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
         dist[0][0] = 0;
-        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
-        pq.push({0, {0, 0}});
+        // {obs, {dist, node}}
+        priority_queue<pair<int, pair<int,pair<int,int>>>, vector<pair<int, pair<int,pair<int,int>>>>, greater<pair<int, pair<int,pair<int,int>>>>> pq;
+        pq.push({0, {0, {0, 0}}});
+        
         while(!pq.empty()) {
-            int cnt = pq.top().first;
+            int obs = pq.top().first;
             int dis = pq.top().second.first;
-            int node = pq.top().second.second;
-            int r = node / m;
-            int c = node % m;
+            int r = pq.top().second.second.first;
+            int c = pq.top().second.second.second;
             pq.pop();
-
+            if(obs > k || (r == n-1 && c == m-1)) continue;
             for(int i=0;i<4;i++) {
                 int nr = r + dx[i], nc = c + dy[i];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                    if(1 + dis < dist[nr][nc]) {
-                        int newCnt = (grid[nr][nc] == 1 ? cnt + 1 : cnt);
-                        int adjNode = nr * m + nc;
-                        if(newCnt <= k) {
-                            dist[nr][nc] = 1 + dis;
-                            pq.push({newCnt, {dist[nr][nc], adjNode}});
-                        }
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m) {
+                    if(dis + 1 < dist[nr][nc]) {
+                        dist[nr][nc] = dis + 1;
+                        if(grid[nr][nc]) pq.push({obs+1, {dist[nr][nc], {nr, nc}}});
+                        else pq.push({obs, {dist[nr][nc], {nr, nc}}});
                     }
                 }
             }
         }
 
-        return (dist[n-1][m-1] == INT_MAX ? -1 : dist[n-1][m-1]);
+        return (dist[n-1][m-1] != INT_MAX ? dist[n-1][m-1] : -1);
     }
 };
