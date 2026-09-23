1353c809a0b52ab286f97f0a05e0d55ef3e2f69c
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-09-23 17:31:27 +0530

    Time: 3 ms (88.99%) | Memory: 8.5 MB (85.67%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    using ll = long long;
-    ll mod = 1e9 + 7;
-    int dx[4] = {1, -1, 0, 0};
-    int dy[4] = {0, 0, -1, 1};
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int mod = 1e9 + 7;
+    int dp[50][50][51];
 
-    ll f(int i, int j, int mm, int n, int m, vector<vector<vector<int>>>&dp) {
+    int f(int i, int j, int moves, int n, int m) {
         if(i < 0 || j < 0 || i >= n || j >= m) return 1;
-        if(!mm) return 0;
-        if(dp[i][j][mm] != -1) return dp[i][j][mm];
-        ll cnt = 0;
+        if(moves == 0) return 0;
+        if(dp[i][j][moves] != -1) return dp[i][j][moves];
+
+        dp[i][j][moves] = 0;
         for(int k=0;k<4;k++) {
-            int ni = i + dx[k], nj = j + dy[k];
-            cnt += f(ni, nj, mm - 1, n, m, dp);
-            cnt %= mod;
+            int r = i + dx[k], c = j + dy[k];
+            dp[i][j][moves] += f(r, c, moves - 1, n, m);
+            dp[i][j][moves] %= mod;
         }
-        return dp[i][j][mm] = cnt;
+        
+        return dp[i][j][moves];
     }
 
-    int findPaths(int n, int m, int mm, int sr, int sc) {
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(mm + 1, -1)));
-        return (int)f(sr, sc, mm, n, m, dp);
+    int findPaths(int n, int m, int mx, int sr, int sc) {
+        memset(dp, -1, sizeof(dp));
+        return f(sr, sc, mx, n, m);
     }
 };
