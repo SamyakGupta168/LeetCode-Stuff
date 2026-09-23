c78ba743c89347bbe87f0818a7941247840f8fae
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-09-23 15:37:42 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (67.64%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-
     int minFallingPathSum(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
-        vector<int> dp(m, -1);
-        for(int j=0;j<m;j++) {
-            dp[j] = mat[0][j];
-        }
-        
+        int n = mat.size();
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+
+        for(int i=0;i<n;i++) dp[0][i] = mat[0][i];
+
         for(int i=1;i<n;i++) {
-            vector<int> temp(m, 1e9);
-            for(int j=0;j<m;j++) {
-                temp[j] = min(temp[j], mat[i][j] + dp[j]);
-                if(j-1 >= 0) temp[j] = min(temp[j], mat[i][j] + dp[j-1]);
-                if(j+1 < m) temp[j] = min(temp[j], mat[i][j] + dp[j+1]);
+            for(int j=0;j<n;j++) {
+                dp[i][j] = min(dp[i][j], dp[i-1][j]);
+                if(j-1 >= 0) dp[i][j] = min(dp[i][j], dp[i-1][j-1]);
+                if(j+1 < n) dp[i][j] = min(dp[i][j], dp[i-1][j+1]);
+                dp[i][j] += mat[i][j];
             }
-            dp = temp;
         }
-        
-        return *min_element(dp.begin(), dp.end());
+
+        return *min_element(dp[n-1].begin(), dp[n-1].end());
     }
 };
