1a4ae97113d2e907272747608a3c41999a6789a8
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-09-23 15:24:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (67.25%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& tt) {
         int n = tt.size();
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        vector<vector<int>> dp(2, vector<int>(n, INT_MAX));
         dp[0][0] = tt[0][0];
         for(int i=1;i<n;i++) {
             for(int j=0;j<=i;j++) {
-                dp[i][j] = tt[i][j] + min(dp[i-1][j], (j-1 >= 0 ? dp[i-1][j-1] : INT_MAX));
+                dp[1][j] = tt[i][j] + min(dp[0][j], (j-1 >= 0 ? dp[0][j-1] : INT_MAX));
             }
+            dp[0] = dp[1];
         }
 
-        return *min_element(dp[n-1].begin(), dp[n-1].end());
+        return *min_element(dp[0].begin(), dp[0].end());
     }
 };
