d40cc41f2fac218b0d25c4fad364af9d9bb0e850
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-09-24 13:37:57 +0530

    Time: 1834 ms (5.87%) | Memory: 117.7 MB (51.20%) - LeetSync

--- 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.cpp
+++ 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& pf) {
+    int minCost(int mxt, vector<vector<int>>& edges, vector<int>& pf) {
         int n = pf.size();
         vector<vector<pair<int,int>>> adj(n);
-        for(auto &x : edges) {
-            int u = x[0], v = x[1], w = x[2];
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+        for(auto &e : edges) {
+            int u = e[0], v = e[1], wt = e[2];
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
         }
 
-        vector<vector<int>> dist(n, vector<int>(maxTime+1, INT_MAX));
-        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
-        dist[0][0] = pf[0];
-        pq.push({pf[0], {0, 0}});
+       vector<vector<int>> dp(n, vector<int>(mxt+1, INT_MAX));
+       dp[0][0] = pf[0];
+       priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
+        pq.push({0, {dp[0][0], 0}});
         while(!pq.empty()) {
-            int cost = pq.top().first;
-            int time = pq.top().second.first;
+            int time = pq.top().first;
+            int cost = pq.top().second.first;
             int node = pq.top().second.second;
             pq.pop();
+            if(dp[node][time] != cost) continue;
 
-            if(node == n-1 || time >= maxTime) continue;
-            if(cost != dist[node][time]) continue;
-
-            for(auto &it : adj[node]) {
-                int adjNode = it.first;
-                int edw = it.second;
-                int newTime = time + edw;
-                if(newTime <= maxTime && cost + pf[adjNode] < dist[adjNode][newTime]) {
-                    dist[adjNode][newTime] = cost + pf[adjNode];
-                    pq.push({dist[adjNode][newTime], {newTime, adjNode}});
+            for(auto &x : adj[node]) {
+                int adjNode = x.first, wt = x.second;
+                if(time + wt > mxt) continue;
+                if(cost + pf[adjNode] < dp[adjNode][time + wt]) {
+                    dp[adjNode][time + wt] = cost + pf[adjNode];
+                    pq.push({time + wt, {dp[adjNode][time + wt], adjNode}});
                 }
             }
         }
 
-        int ans = INT_MAX;
-        for(auto cost : dist[n-1]) ans = min(ans, cost);
+        int ans = *min_element(dp[n-1].begin(), dp[n-1].end());
 
         return (ans != INT_MAX ? ans : -1);
     }
