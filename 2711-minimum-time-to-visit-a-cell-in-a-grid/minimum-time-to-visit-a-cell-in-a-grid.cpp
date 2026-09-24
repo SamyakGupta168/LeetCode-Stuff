a445839ad14f40033c45800303d6bbd2a8ed4e19
SamyakGupta168 <samyakguptakorba@gmail.com>
2026-09-24 12:59:26 +0530

    Time: 192 ms (54.18%) | Memory: 55.5 MB (74.12%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -2,59 +2,37 @@ class Solution {
 public:
     int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
+
     int minimumTime(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        if(grid[1][0] > 1 && grid[0][1] > 1) return -1;
+
+        vector<vector<int>> minTime(n, vector<int>(m, INT_MAX));
+        minTime[0][0] = 0;
         priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
-        dist[0][0] = 0;
         pq.push({0, {0, 0}});
         while(!pq.empty()) {
-            int dis = pq.top().first;
+            int mt = pq.top().first;
             int r = pq.top().second.first;
             int c = pq.top().second.second;
             pq.pop();
+            if(mt != minTime[r][c]) continue;
 
-            int adjTime = INT_MAX;
             for(int i=0;i<4;i++) {
                 int nr = r + dx[i], nc = c + dy[i];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                    adjTime = min(adjTime, grid[nr][nc]);
-                    if(1 + dis >= grid[nr][nc]) {
-                        if(1 + dis < dist[nr][nc]) {
-                            dist[nr][nc] = 1 + dis;
-                            pq.push({dist[nr][nc], {nr, nc}});
-                        }
-                    } else if(r != 0 || c != 0) {
-                        int diff = grid[nr][nc] - dis;
-                        int time = 0;
-                        if(diff & 1) time = dis + diff;
-                        else time = dis + diff + 1;
-                        if(time < dist[nr][nc]) {
-                            dist[nr][nc] = time;
-                            pq.push({dist[nr][nc], {nr, nc}});
-                        }
-                    }
-                }
-            }
-
-            if(adjTime <= 1 && r == 0 && c == 0) {
-                for(int i=0;i<4;i++) {
-                    int nr = r + dx[i], nc = c + dy[i];
-                    if(nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                        if(grid[nr][nc] > 1) {
-                            int time = 0;
-                            if(grid[nr][nc] & 1) time = grid[nr][nc];
-                            else time = grid[nr][nc] + 1;
-                            if(time < dist[nr][nc]) {
-                                dist[nr][nc] = time;
-                                pq.push({dist[nr][nc], {nr, nc}});
-                            }
-                        }
-                    }
+                if(nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
+                int diff = grid[nr][nc] - mt;
+                int time = -1;
+                if(diff <= 0) time = mt + 1;
+                else if(diff & 1) time = grid[nr][nc];
+                else time = grid[nr][nc] + 1;
+                if(time < minTime[nr][nc]) {
+                    minTime[nr][nc] = time;
+                    pq.push({minTime[nr][nc], {nr, nc}});
                 }
             }
         }
 
-        return (dist[n-1][m-1] != INT_MAX ? dist[n-1][m-1] : -1);
+        return (minTime[n-1][m-1] != INT_MAX ? minTime[n-1][m-1] : -1); 
     }
 };
